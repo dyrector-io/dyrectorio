5a6b3639c74affa49c1f54e338f081a630c519ea
Mate Vago <mate.vago.it@gmail.com>
2022-07-12 10:10:57 +0200

    fix: missing recaptcha key

--- web/kratos/entrypoint.sh
+++ web/kratos/entrypoint.sh
@@ -5,6 +5,6 @@ rm -rf /etc/config/kratos/*
 
 cp /usr/bin/app/kratos/identity.schema.json /etc/config/kratos
 cp -r /usr/bin/app/kratos/templates /etc/config/kratos/templates/
-envsubst '${KRATOS_URL}, ${KRATOS_ADMIN_URL}, ${AUTH_URL}' < /usr/bin/app/kratos/kratos.template.yaml > /etc/config/kratos/kratos.yaml
+envsubst '${KRATOS_URL}, ${KRATOS_ADMIN_URL}, ${CRUX_UI_URL}' < /usr/bin/app/kratos/kratos.template.yaml > /etc/config/kratos/kratos.yaml
 
 exec kratos $@
