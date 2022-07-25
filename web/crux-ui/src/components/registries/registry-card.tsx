75ae7cfdf5f1608a86b2304d039c252c2f912153
Szabolcs Szigeti <szszigeti@sunilium.com>
2022-07-25 12:36:04 +0200

    feat(crux-ui): add default registry image

--- web/crux-ui/src/components/registries/registry-card.tsx
+++ web/crux-ui/src/components/registries/registry-card.tsx
@@ -5,6 +5,8 @@ import { DyoLabel } from '@app/elements/dyo-label'
 import { Registry } from '@app/models'
 import clsx from 'clsx'
 import React from 'react'
+import Image from 'next/image'
+import useTranslation from 'next-translate/useTranslation'
 
 interface RegistryCardProps extends Omit<DyoCardProps, 'children'> {
   registry: Registry
@@ -13,14 +15,19 @@ interface RegistryCardProps extends Omit<DyoCardProps, 'children'> {
 
 const RegistryCard = (props: RegistryCardProps) => {
   const { registry, onClick } = props
+  const { t } = useTranslation("registries")
+
+  const getDefaultImage = () => {
+    return <Image src="/default_registry.svg" width={17} height={21} alt={t("altDefaultRegistryPicture")}/>
+  }
 
   return (
     <>
       <DyoCard className={clsx(props.className ?? 'p-6', 'flex flex-col')}>
         <div className={clsx(onClick ? 'cursor-pointer' : null, 'flex flex-row flex-grow')} onClick={onClick}>
-          {!registry.icon ? null : <DyoBadge icon={registry.icon} />}
+          {!registry.icon ? getDefaultImage() : <DyoBadge icon={registry.icon} />}
 
-          <DyoHeading className="text-xl text-bright font-semibold ml-4 my-auto mr-auto" element="h3">
+          <DyoHeading className="text-xl text-bright font-semibold ml-2 my-auto mr-auto" element="h3">
             {registry.name}
           </DyoHeading>
         </div>
