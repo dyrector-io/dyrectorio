a98d4dc7f7d5ccb0ecb704924ac27e6187074c16
Szabolcs Szigeti <48388277+balanceee@users.noreply.github.com>
2022-07-26 10:10:46 +0200

    feat(crux-ui): add type as tag to registry-card (#41)
    * feat(crux, crux-ui): add typetag to registrycard
    
    * remove duplicated import
    
    * fix duplication
    
    * add missing models.ts

--- web/crux-ui/src/components/registries/registry-card.tsx
+++ web/crux-ui/src/components/registries/registry-card.tsx
@@ -2,11 +2,12 @@ import DyoBadge from '@app/elements/dyo-badge'
 import { DyoCard, DyoCardProps } from '@app/elements/dyo-card'
 import { DyoHeading } from '@app/elements/dyo-heading'
 import { DyoLabel } from '@app/elements/dyo-label'
+import DyoTag from '@app/elements/dyo-tag'
 import { Registry } from '@app/models'
 import clsx from 'clsx'
+import useTranslation from 'next-translate/useTranslation'
 import React from 'react'
 import Image from 'next/image'
-import useTranslation from 'next-translate/useTranslation'
 
 interface RegistryCardProps extends Omit<DyoCardProps, 'children'> {
   registry: Registry
@@ -14,8 +15,9 @@ interface RegistryCardProps extends Omit<DyoCardProps, 'children'> {
 }
 
 const RegistryCard = (props: RegistryCardProps) => {
+  const { t } = useTranslation("registries");
+
   const { registry, onClick } = props
-  const { t } = useTranslation("registries")
 
   const getDefaultImage = () => {
     return <Image src="/default_registry.svg" width={17} height={21} alt={t("altDefaultRegistryPicture")}/>
@@ -37,6 +39,12 @@ const RegistryCard = (props: RegistryCardProps) => {
         </div>
 
         <p className="text-light line-clamp-4">{registry.description}</p>
+
+        <div className="flex flex-row flex-grow mt-4 justify-end">
+          <DyoTag color="bg-dyo-turquoise" textColor="text-dyo-turquoise">
+              {t(`type.${registry.type}`)}
+          </DyoTag>
+        </div>
       </DyoCard>
     </>
   )
