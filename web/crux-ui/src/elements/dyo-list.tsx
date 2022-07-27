c472c79a9eff1700e3025647d60b316cf789a64e
Bozóki Viktor <bvektor98@gmail.com>
2022-07-27 15:17:37 +0200

    fix(crux-ui): Auditlog Improvements
    fix: Expand the Name and Date column in Audit Log view
    feat: Make a readonly json editor in the popup modal
    refactor: dyo-list component to use table layout

--- web/crux-ui/src/elements/dyo-list.tsx
+++ web/crux-ui/src/elements/dyo-list.tsx
@@ -25,7 +25,7 @@ export const DyoList = <T,>(props: DyoListProps<T>) => {
     'If the data type is not string, then you must define the itemBuilder',
   )
 
-  const data: Array<string[]> | Array<React.ReactNode> = !props
+  const data: Array<string[]> | Array<React.ReactNode[]> = !props
     ? []
     : isArrayOfStringArrays(props.data)
     ? (props.data as any as string[][])
@@ -45,31 +45,41 @@ export const DyoList = <T,>(props: DyoListProps<T>) => {
       : props.itemClassName ?? headers.map(() => null)
 
   return (
-    <div key={props.key} className={clsx('flex flex-row', props.className)}>
-      {headers.map((header, index) => (
-        <div key={`${props.key}-col-${index}`} className="flex flex-col flex-grow">
-          {!showHeaders ? null : (
-            <div
-              key={`${props.key}-header-${index}`}
-              className={headerClassNames[index] ?? 'text-bright font-bold h-8 mb-4 ml-2 mr-auto'}
-            >
-              {header}
-            </div>
-          )}
-
-          {data.map((it, rowIndex) => (
-            <div
-              key={`${props.key}-${index}-${rowIndex}`}
-              className={clsx(
-                !props.noSeparator ? 'border-t-2 border-light-grey' : null,
-                itemClassNames[index] ?? 'flex flex-grow min-h-12 text-light-eased z-1 p-2 my-auto',
+    <div key={props.key} className={clsx('table w-full', props.className)}>
+      <div className="table-header-group">
+        <div className="table-row">
+          {headers.map((header, index) => (
+            <div key={`${props.key}-col-${index}`} className="table-cell text-left align-middle">
+              {!showHeaders ? null : (
+                <div
+                  key={`${props.key}-header-${index}`}
+                  className={headerClassNames[index] ?? 'text-bright font-bold h-8 mb-4 ml-2 mr-auto'}
+                >
+                  {header}
+                </div>
               )}
-            >
-              {it[index]}
             </div>
           ))}
         </div>
-      ))}
+      </div>
+      <div className="table-row-group">
+        {data.map((line, rowIndex) => (
+          <div className="table-row" key={`${props.key}-${rowIndex}`}>
+            {line.map((_, column) => (
+              <div
+                key={`${props.key}-${column}-${rowIndex}`}
+                className={clsx(
+                  'table-cell align-middle',
+                  !props.noSeparator ? 'border-t-2 border-light-grey' : null,
+                  itemClassNames[column] ?? 'h-12 min-h-min text-light-eased p-2',
+                )}
+              >
+                {line[column]}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- web/crux-ui/src/pages/teams/audit-log.tsx
+++ web/crux-ui/src/pages/teams/audit-log.tsx
@@ -1,5 +1,6 @@
 import { Layout, PageHead } from '@app/components/layout'
 import { BreadcrumbLink } from '@app/components/shared/breadcrumb'
+import JsonEditor from '@app/components/shared/json-editor-dynamic-module'
 import PageHeading from '@app/components/shared/page-heading'
 import { DyoCard } from '@app/elements/dyo-card'
 import { DyoList } from '@app/elements/dyo-list'
@@ -34,7 +35,7 @@ const AuditLogPage = (props: AuditLogPageProps) => {
   const listHeaders = ['common:name', 'common:date', 'event', 'info'].map(it => t(it))
   const defaultHeaderClass = 'uppercase text-bright text-sm font-bold bg-medium-eased pl-2 py-3 h-11'
   const headerClassNames = [
-    clsx(defaultHeaderClass, 'rounded-tl-lg pl-16'),
+    clsx(defaultHeaderClass, 'rounded-tl-lg w-20 min-w-full pl-4'),
     ...Array.from({ length: listHeaders.length - 2 }).map(() => defaultHeaderClass),
     clsx(defaultHeaderClass, 'rounded-tr-lg pr-16'),
   ]
@@ -54,8 +55,8 @@ const AuditLogPage = (props: AuditLogPageProps) => {
           itemBuilder={it => {
             /* eslint-disable react/jsx-key */
             return [
-              <div className="font-semibold ml-14 py-1 h-8">{it.identityName}</div>,
-              <div>{utcDateToLocale(it.date)}</div>,
+              <div className="font-semibold min-w-max pl-2">{it.identityName}</div>,
+              <div className="min-w-max">{utcDateToLocale(it.date)}</div>,
               <div>{beautifyAuditLogEvent(it.event)}</div>,
               <div className="cursor-pointer" onClick={() => onShowInfoClick(it)}>
                 {it.info}
@@ -73,11 +74,12 @@ const AuditLogPage = (props: AuditLogPageProps) => {
           open={!!showInfo}
           onClose={() => setShowInfo(null)}
         >
-          <>
-            <span className="text-bright font-semibold">{beautifyAuditLogEvent(showInfo.event)}</span>
-
-            <p className="text-bright mt-8 overflow-y-auto">{showInfo.info}</p>
-          </>
+          <span className="text-bright font-semibold">{beautifyAuditLogEvent(showInfo.event)}</span>
+          <JsonEditor
+            className="text-bright mt-8 overflow-y-auto h-full !pointer-events-auto"
+            disabled
+            value={JSON.parse(showInfo.info)}
+          />
         </DyoModal>
       )}
     </Layout>
