ea3b72905a836031664f084a802a3d7ef763eb0e
Bence Nagygyörgy <robot9706@gmail.com>
2022-08-01 10:26:47 +0200

    refactor: removed dogger error tests & changed assertion

--- agent/internal/dogger/dogger_test.go
+++ agent/internal/dogger/dogger_test.go
@@ -5,7 +5,6 @@ package dogger_test
 
 import (
 	"context"
-	"errors"
 	"testing"
 
 	"github.com/dyrector-io/dyrectorio/agent/internal/config"
@@ -16,155 +15,105 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-type AssertFunc func(*testing.T, *crux.DeploymentStatusMessage) error
-
 type AgentTestStream struct {
-	testing   *testing.T
-	AssertLog AssertFunc
+	testing  *testing.T
+	messages []string
 }
 
-func (tStream AgentTestStream) Send(message *crux.DeploymentStatusMessage) error {
+func (tStream *AgentTestStream) Send(message *crux.DeploymentStatusMessage) error {
 	tStream.testing.Logf("Dogger said: '%s'", message.Log)
-	return tStream.AssertLog(tStream.testing, message)
+	tStream.messages = append(tStream.messages, message.Log...)
+	return nil
 }
 
-func (tStream AgentTestStream) CloseAndRecv() (*agent.Empty, error) {
+func (tStream *AgentTestStream) CloseAndRecv() (*agent.Empty, error) {
 	return nil, nil
 }
 
-func (tStream AgentTestStream) CloseSend() error {
+func (tStream *AgentTestStream) CloseSend() error {
 	return nil
 }
 
-func (tStream AgentTestStream) Header() (metadata.MD, error) {
+func (tStream *AgentTestStream) Header() (metadata.MD, error) {
 	return nil, nil
 }
 
-func (tStream AgentTestStream) Trailer() metadata.MD {
+func (tStream *AgentTestStream) Trailer() metadata.MD {
 	return nil
 }
 
-func (tStream AgentTestStream) Context() context.Context {
+func (tStream *AgentTestStream) Context() context.Context {
 	return nil
 }
 
-func (tStream AgentTestStream) SendMsg(m any) error {
+func (tStream *AgentTestStream) SendMsg(m any) error {
 	return nil
 }
 
-func (tStream AgentTestStream) RecvMsg(m any) error {
+func (tStream *AgentTestStream) RecvMsg(m any) error {
 	return nil
+}
+
+func assertDoggerMessage(t *testing.T, ts *AgentTestStream, messages []string) {
+	assert.Equal(t, len(messages), len(ts.messages))
+
+	for _, value := range messages {
+		assert.Contains(t, ts.messages, value)
+	}
 }
 
 func TestDoggerSingle(t *testing.T) {
 	cfg := &config.CommonConfiguration{}
 	deploymentId := "abcd"
 
-	ts := AgentTestStream{
+	ts := &AgentTestStream{
 		testing: t,
-		AssertLog: func(t *testing.T, message *crux.DeploymentStatusMessage) error {
-			expected := []string{"hello"}
-			assert.Equal(t, expected, message.Log)
-			return nil
-		},
 	}
 	dogger := dogger.NewDeploymentLogger(&deploymentId, ts, context.TODO(), cfg)
 
 	dogger.Write("hello")
+
+	assertDoggerMessage(t, ts, []string{"hello"})
 }
 
 func TestDoggerMuliple(t *testing.T) {
 	cfg := &config.CommonConfiguration{}
 	deploymentId := "abcd"
 
-	ts := AgentTestStream{
+	ts := &AgentTestStream{
 		testing: t,
-		AssertLog: func(t *testing.T, message *crux.DeploymentStatusMessage) error {
-			expected := []string{"hello", "abcd"}
-			assert.Equal(t, expected, message.Log)
-			return nil
-		},
 	}
 	dogger := dogger.NewDeploymentLogger(&deploymentId, ts, context.TODO(), cfg)
 
 	dogger.Write("hello", "abcd")
-}
 
-func TestDoggerErrorTest(t *testing.T) {
-	cfg := &config.CommonConfiguration{}
-	deploymentId := "abcd"
-
-	ts := AgentTestStream{
-		testing: t,
-		AssertLog: func(t *testing.T, message *crux.DeploymentStatusMessage) error {
-			return errors.New("Test error")
-		},
-	}
-	dogger := dogger.NewDeploymentLogger(&deploymentId, ts, context.TODO(), cfg)
-
-	dogger.Write("hello")
+	assertDoggerMessage(t, ts, []string{"hello", "abcd"})
 }
 
 func TestDoggerDeploymentStatus(t *testing.T) {
 	cfg := &config.CommonConfiguration{}
 	deploymentId := "abcd"
 
-	ts := AgentTestStream{
+	ts := &AgentTestStream{
 		testing: t,
-		AssertLog: func(t *testing.T, message *crux.DeploymentStatusMessage) error {
-			expected := []string{"hello"}
-			assert.Equal(t, expected, message.Log)
-			return nil
-		},
 	}
 	dogger := dogger.NewDeploymentLogger(&deploymentId, ts, context.TODO(), cfg)
 
 	dogger.WriteDeploymentStatus(crux.DeploymentStatus_IN_PROGRESS, "hello")
-}
 
-func TestDoggerDeploymentStatusError(t *testing.T) {
-	cfg := &config.CommonConfiguration{}
-	deploymentId := "abcd"
-
-	ts := AgentTestStream{
-		testing: t,
-		AssertLog: func(t *testing.T, message *crux.DeploymentStatusMessage) error {
-			return errors.New("Test error")
-		},
-	}
-	dogger := dogger.NewDeploymentLogger(&deploymentId, ts, context.TODO(), cfg)
-
-	dogger.WriteDeploymentStatus(crux.DeploymentStatus_IN_PROGRESS, "hello")
+	assertDoggerMessage(t, ts, []string{"hello"})
 }
 
 func TestDoggerContainerStatus(t *testing.T) {
 	cfg := &config.CommonConfiguration{}
 	deploymentId := "abcd"
 
-	ts := AgentTestStream{
+	ts := &AgentTestStream{
 		testing: t,
-		AssertLog: func(t *testing.T, message *crux.DeploymentStatusMessage) error {
-			expected := []string{"hello"}
-			assert.Equal(t, expected, message.Log)
-			return nil
-		},
 	}
 	dogger := dogger.NewDeploymentLogger(&deploymentId, ts, context.TODO(), cfg)
 
 	dogger.WriteContainerStatus("state", "hello")
-}
 
-func TestDoggerContainerStatusError(t *testing.T) {
-	cfg := &config.CommonConfiguration{}
-	deploymentId := "abcd"
-
-	ts := AgentTestStream{
-		testing: t,
-		AssertLog: func(t *testing.T, message *crux.DeploymentStatusMessage) error {
-			return errors.New("Test error")
-		},
-	}
-	dogger := dogger.NewDeploymentLogger(&deploymentId, ts, context.TODO(), cfg)
-
-	dogger.WriteContainerStatus("state", "hello")
+	assertDoggerMessage(t, ts, []string{"hello"})
 }
