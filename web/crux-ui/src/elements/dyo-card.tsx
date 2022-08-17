d2d5a7d47cef35d823f22abf253390fb8d818f0c
Levente Orban <levente.orban@dyrector.io>
2022-08-17 15:44:01 +0200

    fix(crux-ui): add diff box-shadow and improve minor stylistic issues

--- web/crux-ui/src/elements/dyo-card.tsx
+++ web/crux-ui/src/elements/dyo-card.tsx
@@ -10,11 +10,5 @@ export interface DyoCardProps extends RefAttributes<HTMLDivElement> {
 export const DyoCard = (props: DyoCardProps) => {
   const boxShadow = props.modal ? 'shadow-modal' : 'shadow-lg'
 
-  return (
-    <div
-      className={clsx(props.className ?? 'p-8', 'card rounded-lg bg-medium', boxShadow)}
-    >
-      {props.children}
-    </div>
-  )
+  return <div className={clsx(props.className ?? 'p-8', 'card rounded-lg bg-medium', boxShadow)}>{props.children}</div>
 }
