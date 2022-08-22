fbe0e918495e6d91278fe9439b13df99dbf47f32
Levente Orban <levente.orban@dyrector.io>
2022-08-22 12:44:46 +0200

    fix: i18n improvement & remove unused type import & eslintrc vscode fix

--- web/crux-ui/.eslintrc.js
+++ web/crux-ui/.eslintrc.js
@@ -1,18 +1,12 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
-  extends: [
-    'next/babel',
-    'next/core-web-vitals',
-    'prettier',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-  ],
+  extends: ['next/core-web-vitals', 'prettier', 'eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended'],
   rules: {
     'react-hooks/rules-of-hooks': 'off',
     'no-console': [
       'error',
       {
-        allow: ['error', 'trace', 'info', 'debug', 'warn'],
+        allow: ['error','trace','info','debug','warn'],
       },
     ],
     'no-unused-vars': 'off',
