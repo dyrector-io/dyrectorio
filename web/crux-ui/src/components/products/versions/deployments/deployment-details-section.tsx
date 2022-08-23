3cb2b59484f363135ee99699a844c9bbeafbddb1
Mate Vago <mate.vago.it@gmail.com>
2022-08-23 13:18:49 +0200

    fix: json editor scrolling

--- web/crux-ui/src/components/products/versions/deployments/deployment-details-section.tsx
+++ web/crux-ui/src/components/products/versions/deployments/deployment-details-section.tsx
@@ -1,6 +1,6 @@
 import KeyValueInput from '@app/components/shared/key-value-input'
 import { DEPLOYMENT_EDIT_WS_REQUEST_DELAY } from '@app/const'
-import { useThrottleing } from '@app/hooks/use-throttleing'
+import { useThrottling } from '@app/hooks/use-throttleing'
 import { deploymentIsMutable, DeploymentRoot, Environment, WS_TYPE_PATCH_DEPLOYMENT_ENV } from '@app/models'
 import { WebSocketEndpoint } from '@app/websockets/client'
 import useTranslation from 'next-translate/useTranslation'
@@ -18,7 +18,7 @@ const DeploymentDetailsSection = (props: DeploymentDetailsSectionProps) => {
 
   const mutable = deploymentIsMutable(deployment.status)
 
-  const throttle = useThrottleing(DEPLOYMENT_EDIT_WS_REQUEST_DELAY)
+  const throttle = useThrottling(DEPLOYMENT_EDIT_WS_REQUEST_DELAY)
 
   const onEnvChange = (env: Environment) => throttle(() => sock.send(WS_TYPE_PATCH_DEPLOYMENT_ENV, env))
   return (
