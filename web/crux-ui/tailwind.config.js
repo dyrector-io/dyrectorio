ed2f4eec803b5e4f1c3a6670e406b2d1da40846d
Peter Ambrus <peter.ambrus3@gmail.com>
2022-08-29 16:30:27 +0200

    fix(crux-ui): white error toast
    The error toast was white because of a wrong style.

--- web/crux-ui/tailwind.config.js
+++ web/crux-ui/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}',
-        './src/elements/**/*.{js,ts,jsx,tsx}'
+        './src/elements/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {
@@ -10,13 +10,13 @@ module.exports = {
                 'dyo-turquoise': '#00d0bf',
                 'dyo-green': '#28c76f',
                 'dyo-purple': '#540BA7',
-                'bright': "#d0d2d6",
-                'bright-muted': "#676D7D",
+                'bright': '#d0d2d6',
+                'bright-muted': '#676D7D',
                 'light-eased': '#b4b7bd',
                 'light-grey': '#3b4253',
                 'light-grey-muted': '#404656',
-                'light': "#7783a3",
-                'medium-eased': "#343d55",
+                'light': '#7783a3',
+                'medium-eased': '#343d55',
                 'medium': '#283046',
                 'dark': '#161d31',
                 'error-red': '#ea5455',
@@ -27,24 +27,22 @@ module.exports = {
                 'dyo-light-purple-pale': '#DEDBEB',
                 'dyo-light-purple-pale-shadow': '#DCE2F3',
                 'dyo-eased-purple': '#796EB9',
-                'dyo-eased-purple-pale': "#CDD6EF",
+                'dyo-eased-purple-pale': '#CDD6EF',
                 'dyo-dark': '#1E1A33',
             },
             height: {
-                '128': '32rem'
+                '128': '32rem',
             },
             maxHeight: {
-                '128': '32rem'
+                '128': '32rem',
             },
             boxShadow: {
                 'modal': '0 0 20px 5px rgb(0 0 0 / 0.25)',
-            }
+            },
         },
     },
     variants: {
         extend: {},
     },
-    plugins: [
-        require('@tailwindcss/line-clamp'),
-    ]
+    plugins: [require('@tailwindcss/line-clamp')],
 }
