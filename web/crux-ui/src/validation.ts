bd7ea5d4950d8e84d55c67d407571a7db46cb013
Bence Nagygyörgy <robot9706@gmail.com>
2022-08-29 14:36:07 +0200

    fix: review changes

--- web/crux-ui/src/validation.ts
+++ web/crux-ui/src/validation.ts
@@ -63,9 +63,9 @@ export const createProductSchema = updateProductSchema.concat(
 )
 
 const registryCredentialRole = yup.mixed().when(['type', '_private'], {
-  is: (type, _private) => ['gitlab', 'github'].includes(type) || ((type === 'v2' || type === 'google') && _private),
+  is: (type, _private) => (type === 'gitlab' || type === 'github') || ((type === 'v2' || type === 'google') && _private),
   then: yup.string().required(),
-  otherwise: yup.mixed().transform(value => value ? value : undefined)
+  otherwise: yup.mixed().transform(it => it ? it : undefined)
 })
 
 const googleRegistryUrls = ['gcr.io', 'us.gcr.io', 'eu.gcr.io', 'asia.gcr.io'] as const
