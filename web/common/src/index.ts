09b53f6e903aa29104d38c2fd1cd2e7956911ade
Bence Nagygyörgy <robot9706@gmail.com>
2022-08-30 14:42:18 +0200

    feat: added prettier to common

--- web/common/src/index.ts
+++ web/common/src/index.ts
@@ -1,12 +1,44 @@
-import { UniqueKeyValue, EXPLICIT_CONTAINER_NETWORK_MODE_VALUES, ExplicitContainerNetworkMode, EXPLICIT_CONTAINER_RESTART_POLICY_TYPE_VALUES, ExplicitContainerRestartPolicyType, 
-    EXPLICIT_CONTAINER_DEPLOYMENT_STRATEGY_VALUES, ExplicitContainerDeploymentStrategyType, ContainerConfigData, InstanceContainerConfigData, 
-    IdentityTraits, IdentityTraitsName, nameOfIdentity, nameOrEmailOfIdentity } from './model'
-import { uniqueKeyValuesSchema, explicitContainerConfigSchema, containerConfigSchema, deploymentSchema } from './validation'
+import {
+  UniqueKeyValue,
+  EXPLICIT_CONTAINER_NETWORK_MODE_VALUES,
+  ExplicitContainerNetworkMode,
+  EXPLICIT_CONTAINER_RESTART_POLICY_TYPE_VALUES,
+  ExplicitContainerRestartPolicyType,
+  EXPLICIT_CONTAINER_DEPLOYMENT_STRATEGY_VALUES,
+  ExplicitContainerDeploymentStrategyType,
+  ContainerConfigData,
+  InstanceContainerConfigData,
+  IdentityTraits,
+  IdentityTraitsName,
+  nameOfIdentity,
+  nameOrEmailOfIdentity,
+} from './model'
+import {
+  uniqueKeyValuesSchema,
+  explicitContainerConfigSchema,
+  containerConfigSchema,
+  deploymentSchema,
+} from './validation'
 
 export type {
-    ExplicitContainerDeploymentStrategyType, ContainerConfigData, InstanceContainerConfigData, IdentityTraitsName, IdentityTraits, UniqueKeyValue, ExplicitContainerNetworkMode,
-    ExplicitContainerRestartPolicyType
+  ExplicitContainerDeploymentStrategyType,
+  ContainerConfigData,
+  InstanceContainerConfigData,
+  IdentityTraitsName,
+  IdentityTraits,
+  UniqueKeyValue,
+  ExplicitContainerNetworkMode,
+  ExplicitContainerRestartPolicyType,
 }
 
-export { uniqueKeyValuesSchema, explicitContainerConfigSchema, containerConfigSchema, deploymentSchema, EXPLICIT_CONTAINER_NETWORK_MODE_VALUES, 
-    EXPLICIT_CONTAINER_RESTART_POLICY_TYPE_VALUES, EXPLICIT_CONTAINER_DEPLOYMENT_STRATEGY_VALUES, nameOfIdentity, nameOrEmailOfIdentity }
+export {
+  uniqueKeyValuesSchema,
+  explicitContainerConfigSchema,
+  containerConfigSchema,
+  deploymentSchema,
+  EXPLICIT_CONTAINER_NETWORK_MODE_VALUES,
+  EXPLICIT_CONTAINER_RESTART_POLICY_TYPE_VALUES,
+  EXPLICIT_CONTAINER_DEPLOYMENT_STRATEGY_VALUES,
+  nameOfIdentity,
+  nameOrEmailOfIdentity,
+}
