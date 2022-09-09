d25fe12cc2146fd8d3211c2a51016af622505c78
Nandor Magyar <nandormagyar.it@gmail.com>
2022-09-09 14:39:49 +0000

    feat(agent): add wait until into builder (#154)
    * feat(agent): add wait until into builder
    * clean(agent): removed double logging info message

--- agent/pkg/dagent/utils/docker.go
+++ agent/pkg/dagent/utils/docker.go
@@ -360,6 +360,7 @@ func DeployImage(ctx context.Context,
 		WithUser(deployImageRequest.ContainerConfig.User).
 		WithEntrypoint(deployImageRequest.ContainerConfig.Command).
 		WithCmd(deployImageRequest.ContainerConfig.Args).
+		WithoutConflict().
 		WithLogWriter(dog)
 
 	WithImportContainer(builder, deployImageRequest.ContainerConfig.ImportContainer, dog, cfg)
@@ -405,9 +406,10 @@ func WithImportContainer(dc *containerbuilder.DockerContainerBuilder, importConf
 			mountList []mount.Mount,
 			logger *io.StringWriter) error {
 			if initError := spawnInitContainer(client, ctx, containerName, mountList, importConfig, dog, cfg); initError != nil {
-				log.Printf("Failed to spawn init container: %v", initError)
+				dog.WriteDeploymentStatus(common.DeploymentStatus_FAILED, "Failed to spawn init container: "+initError.Error())
 				return initError
 			}
+			dog.WriteDeploymentStatus(common.DeploymentStatus_IN_PROGRESS, "Loading assets was successful.")
 			return nil
 		})
 	}
