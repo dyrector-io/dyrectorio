db65f48e9b37df1008a8e2515744645190981f49
Bence Nagygyörgy <robot9706@gmail.com>
2022-09-14 13:21:06 +0200

    feat(web): validation & merged migrations

--- web/crux/prisma/migrations/20220914101732_deployment_refactor/migration.sql
+++ web/crux/prisma/migrations/20220914101732_deployment_refactor/migration.sql
@@ -5,6 +5,7 @@
   - A unique constraint covering the columns `[versionId,nodeId,prefix]` on the table `Deployment` will be added. If there are existing duplicate values, this will fail.
 
 */
+
 -- DropIndex
 DROP INDEX "Deployment_versionId_name_key";
 
@@ -13,3 +14,18 @@ ALTER TABLE "Deployment" DROP COLUMN "name";
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Deployment_versionId_nodeId_prefix_key" ON "Deployment"("versionId", "nodeId", "prefix");
+
+-- Copy data
+DROP TABLE IF EXISTS public._migration_deployment;
+SELECT
+  id, "description"
+INTO public._migration_deployment
+FROM public."Deployment";
+
+-- AlterTable
+ALTER TABLE "Deployment" DROP COLUMN "description",
+ADD COLUMN     "note" TEXT;
+
+-- Update values
+UPDATE "Deployment" SET "note" = "_migration_deployment"."description"
+FROM "_migration_deployment" WHERE "_migration_deployment".id = "Deployment".id;
