65679ba2d1e40e6a57df2f19d5aece3a26dedb20
Bozóki Viktor <bvektor98@gmail.com>
2022-09-27 15:42:04 +0200

    fix(crux-ui): review suggestions

--- web/crux-ui/src/elements/dyo-filter-chips.tsx
+++ web/crux-ui/src/elements/dyo-filter-chips.tsx
@@ -2,15 +2,17 @@ import useTranslation from 'next-translate/useTranslation'
 import DyoChips, { DyoChipsProps } from './dyo-chips'
 
 const DyoFilterChips = <T,>(props: DyoChipsProps<T | 'all'>) => {
-  const { converter, choices, initialSelection } = props
+  const { converter: propsConverter, choices, initialSelection } = props
 
   const { t } = useTranslation('common')
+
+  const converter = (it: T | 'all') => (it === 'all' ? t('all') : propsConverter(it))
 
   return (
     <DyoChips
       {...props}
-      converter={it => (it === 'all' ? t('all') : converter(it))}
-      choices={['all' as 'all', ...choices]}
+      converter={converter}
+      choices={['all', ...choices]}
       initialSelection={initialSelection ?? 'all'}
     />
   )
