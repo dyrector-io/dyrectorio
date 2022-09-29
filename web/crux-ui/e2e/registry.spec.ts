eb50833802db8a50a9feeb52b8d32416726cce4c
Bence Nagygyörgy <robot9706@gmail.com>
2022-09-29 14:34:58 +0200

    refactor(crux-ui): working on registry tests

---
+++ web/crux-ui/e2e/registry.spec.ts
@@ -0,0 +1,54 @@
+import { ROUTE_LOGIN, ROUTE_REGISTRIES } from '@app/routes'
+import { expect, test } from '@playwright/test'
+import { screenshotPath, USER_EMAIL, USER_PASSWORD } from './utils/common'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(ROUTE_LOGIN)
+
+  await page.locator('input[name=email]').fill(USER_EMAIL)
+  await page.locator('input[name=password]').fill(USER_PASSWORD)
+
+  await page.locator('button[type=submit]').click()
+
+  await page.screenshot({ path: screenshotPath('login-successful'), fullPage: true })
+
+  await expect(page).toHaveURL(`/products`)
+})
+
+test('adding a new registry should work', async ({ page }) => {
+  await page.goto(ROUTE_REGISTRIES)
+
+  await page.locator('text=Add').click();
+  await expect(page.locator('h4')).toContainText('New registry')
+
+  await page.locator('input[name=name]').fill('TEST REGISTRY')
+  await page.locator('form >> text=Docker Hub').click();
+  await expect(await page.locator('label[for=imageNamePrefix]')).toContainText('Organization name or username')
+  await page.locator('input[name=imageNamePrefix]').fill('library')
+
+  await page.screenshot({ path: screenshotPath('registry_new_filled'), fullPage: true })
+
+  await page.locator('text=Save').click();
+
+  await page.waitForURL(ROUTE_REGISTRIES)
+
+  await page.screenshot({ path: screenshotPath('registry_new'), fullPage: true })
+})
+
+test('minimum name length requirement should work', async ({ page }) => {
+  await page.goto(ROUTE_REGISTRIES)
+
+  await page.locator('text=Add').click();
+  await expect(page.locator('h4')).toContainText('New registry')
+
+  await page.locator('input[name=name]').fill('12')
+  await page.locator('form >> text=Docker Hub').click();
+  await expect(await page.locator('label[for=imageNamePrefix]')).toContainText('Organization name or username')
+  await page.locator('input[name=imageNamePrefix]').fill('library')
+
+  await page.locator('text=Save').click();
+
+  await expect(await page.locator('p.text-error-red')).toContainText('name must be at least 3 characters')
+
+  await page.screenshot({ path: screenshotPath('registry_new_name_length'), fullPage: true })
+})
