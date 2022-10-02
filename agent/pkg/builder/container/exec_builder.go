80285e8a07caf49652a1211b02a0d74e56f864df
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-10-02 20:27:28 +0530

    feat: add doc for functions in container exec builder

--- agent/pkg/builder/container/exec_builder.go
+++ agent/pkg/builder/container/exec_builder.go
@@ -11,6 +11,9 @@ import (
 	"github.com/docker/docker/client"
 )
 
+// An ExecBuilder handles the process of creating and starting exec,
+// it can be configured using 'With...' methods.
+// An ExecBuilder can be created using the DockerExecBuilder method.
 type ExecBuilder interface {
 	WithAttachStderr() *DockerExecBuilder
 	WithAttachStdin() *DockerExecBuilder
@@ -44,6 +47,9 @@ type DockerExecBuilder struct {
 	privileged   bool
 }
 
+// A shorthand function for creating a new DockerExecBuilder and calling WithClient.
+// Creates a default Docker client which can be overwritten using 'WithClient'.
+// Creates a default logger which logs using the 'fmt' package.
 func NewExecBuilder(ctx context.Context, containerID string) *DockerExecBuilder {
 	var logger io.StringWriter = &defaultLogger{}
 	b := DockerExecBuilder{
@@ -60,61 +66,73 @@ func NewExecBuilder(ctx context.Context, containerID string) *DockerExecBuilder 
 	return b.WithClient(cli)
 }
 
+// Sets the builder to attach stderr to the exec command
 func (de *DockerExecBuilder) WithAttachStderr() *DockerExecBuilder {
 	de.attachStderr = true
 	return de
 }
 
+// Sets the builder to attach stdin to the exec command
 func (de *DockerExecBuilder) WithAttachStdin() *DockerExecBuilder {
 	de.attachStdin = true
 	return de
 }
 
+// Sets the builder to attach stdout to the exec command
 func (de *DockerExecBuilder) WithAttachStdout() *DockerExecBuilder {
 	de.attachStdout = true
 	return de
 }
 
+// Sets the Docker client of the ExecBuilder. By default NewExecBuilder creates a client.
 func (de *DockerExecBuilder) WithClient(cli *client.Client) *DockerExecBuilder {
 	de.client = cli
 	return de
 }
 
+// Sets the commands to run as part of exec
 func (de *DockerExecBuilder) WithCmd(cmd []string) *DockerExecBuilder {
 	de.cmd = cmd
 	return de
 }
 
+// Sets the builder to detach from the exec command
 func (de *DockerExecBuilder) WithDetach() *DockerExecBuilder {
 	de.detach = true
 	return de
 }
 
+// Sets the logger which logs messages releated to the builder (and not the container).
 func (de *DockerExecBuilder) WithLogWriter(logger io.StringWriter) *DockerExecBuilder {
 	de.logger = &logger
 	return de
 }
 
+// Sets the builder to run the exec process with extended privileges.
 func (de *DockerExecBuilder) WithPrivileged() *DockerExecBuilder {
 	de.privileged = true
 	return de
 }
 
+// Sets if standard streams should be attached to a tty.
 func (de *DockerExecBuilder) WithTTY() *DockerExecBuilder {
 	de.tty = true
 	return de
 }
 
+// Sets the UID.
 func (de *DockerExecBuilder) WithUser(user string) *DockerExecBuilder {
 	de.user = user
 	return de
 }
 
+//Sets the working directory for the exec process inside the container.
 func (de *DockerExecBuilder) WithWorkingDir(workingDir string) *DockerExecBuilder {
 	de.workingDir = workingDir
 	return de
 }
 
+// Creates the exec command using the configuration given by 'With...' functions.
 func (de *DockerExecBuilder) Create() *DockerExecBuilder {
 	execConfig := types.ExecConfig{
 		User:         de.user,
@@ -138,6 +156,8 @@ func (de *DockerExecBuilder) Create() *DockerExecBuilder {
 	return de
 }
 
+// Runs the container using the configuration given by 'With...' functions.
+// Returns true if successful, false and an error if not.
 func (de *DockerExecBuilder) Start() (bool, error) {
 	execStartCheck := types.ExecStartCheck{
 		Detach: de.detach,
