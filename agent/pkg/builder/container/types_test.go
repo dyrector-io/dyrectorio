ae7b042c4f166bb0ae5f45c959c083fb3fcaf188
Bence <robot9706@gmail.com>
2022-10-03 11:32:49 +0200

    refactor(agent): adding tests to agent (#213)

---
+++ agent/pkg/builder/container/types_test.go
@@ -0,0 +1,56 @@
+package container_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dyrector-io/dyrectorio/agent/pkg/builder/container"
+)
+
+func TestRestartPolicyNameMarshal(t *testing.T) {
+	testPolicies := map[container.RestartPolicyName]string{
+		container.EmptyRestartPolicy:                "unless-stopped",
+		container.RestartUnlessStoppedRestartPolicy: "unless-stopped",
+		container.NoRestartPolicy:                   "no",
+		container.AlwaysRestartPolicy:               "always",
+		container.OnFailureRestartPolicy:            "on-failure",
+	}
+
+	for key, value := range testPolicies {
+		text, err := json.Marshal(key)
+
+		expectedResult := fmt.Sprintf("%q", value)
+
+		assert.Nil(t, err)
+		assert.Equal(t, expectedResult, string(text))
+	}
+}
+
+func TestRestartPolicyNameUnmarshal(t *testing.T) {
+	testPolicies := map[string]container.RestartPolicyName{
+		"":               container.RestartUnlessStoppedRestartPolicy,
+		"unless-stopped": container.RestartUnlessStoppedRestartPolicy,
+		"no":             container.NoRestartPolicy,
+		"always":         container.AlwaysRestartPolicy,
+		"on-failure":     container.OnFailureRestartPolicy,
+	}
+
+	for key, value := range testPolicies {
+		var parsed container.RestartPolicyName
+		jsonValue := fmt.Sprintf("%q", key)
+		err := json.Unmarshal([]byte(jsonValue), &parsed)
+
+		assert.Nil(t, err)
+		assert.Equal(t, value, parsed)
+	}
+}
+
+func TestRestartPolicyNameUnmarshalError(t *testing.T) {
+	var parsed container.RestartPolicyName
+	err := json.Unmarshal([]byte("\"not-valid-policy-name\""), &parsed)
+
+	assert.ErrorIs(t, err, &container.ErrRestartPolicyUnmarshalInvalid{})
+}
--- agent/pkg/builder/container/utils_test.go
+++ agent/pkg/builder/container/utils_test.go
@@ -4,8 +4,12 @@
 package container_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 	"github.com/stretchr/testify/assert"
 
 	builder "github.com/dyrector-io/dyrectorio/agent/pkg/builder/container"
@@ -33,4 +37,43 @@ func TestRegistryAuthUpdateBase64Correct(t *testing.T) {
 	val := builder.RegistryAuthBase64("test", "test1234")
 
 	assert.Equal(t, "eyJ1c2VybmFtZSI6InRlc3QiLCJwYXNzd29yZCI6InRlc3QxMjM0In0=", val)
+}
+
+func TestPullImage(t *testing.T) {
+	logger := &TestLogger{
+		test: t,
+	}
+
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{
+		Filters: filters.NewArgs(filters.KeyValuePair{Key: "reference", Value: "nginx"}),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, image := range images {
+		cli.ImageRemove(context.Background(), image.ID, types.ImageRemoveOptions{
+			Force: true,
+		})
+	}
+
+	err = builder.PullImage(context.Background(), logger, "nginx:latest", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	images, err = cli.ImageList(context.Background(), types.ImageListOptions{
+		Filters: filters.NewArgs(filters.KeyValuePair{Key: "reference", Value: "nginx"}),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.True(t, logger.gotMessage)
+	assert.Greater(t, len(images), 0)
 }
