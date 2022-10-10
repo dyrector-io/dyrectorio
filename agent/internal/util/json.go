0fdd27d390a3b5e2e9d79306702a46dc8deda40b
Borja Clemente <borja.clemente@gmail.com>
2022-10-10 13:10:17 +0200

    Simplify trailing whitespace regex group
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

--- agent/internal/util/json.go
+++ agent/internal/util/json.go
@@ -7,7 +7,7 @@ import (
 func RemoveJSONComment(strIn []byte) []byte {
 	str := string(strIn)
 	// edgecase that isn't handled: double quote in the comment
-	re1 := regexp.MustCompile(`([ \t]*)//\s*?[^"\\S]*?\s*?(\n|\r|\r\n|\n\r)`)
+	re1 := regexp.MustCompile(`([\s]*)//\s*?[^"\\S]*?\s*?(\n|\r|\r\n|\n\r)`)
 	str = re1.ReplaceAllString(str, "")
 
 	return []byte(str)
