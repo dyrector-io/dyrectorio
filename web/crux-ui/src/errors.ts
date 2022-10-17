209f5ba5fec6c936ead46b26c0fa9c69bea9b620
R C N <plbwymw@gmail.com>
2022-10-17 23:26:39 +0300

    chore: missed a few try-catch(s)

--- web/crux-ui/src/errors.ts
+++ web/crux-ui/src/errors.ts
@@ -40,7 +40,7 @@ export const apiErrorHandler =
       if (setErrorValue && translation.input) {
         setErrorValue(dto.property, translation.input)
       }
-    } catch (e) {
+    } catch (err) {
       translation = translator(null, 500, null)
     }
 
