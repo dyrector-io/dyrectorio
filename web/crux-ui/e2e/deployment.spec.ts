bcf4f90d969d98220616f01ea01db91bc8b7d2e5
Mate Vago <mate.vago.it@gmail.com>
2022-11-17 13:37:10 +0100

    fix: create multiple preparing deployments to the same node with different prefixes

--- web/crux-ui/e2e/deployment.spec.ts
+++ web/crux-ui/e2e/deployment.spec.ts
@@ -61,3 +61,23 @@ test('preparing deployment is copyable on deployment detail page', async ({ page
 
   await expect(await page.locator('button:has-text("Copy")')).toHaveCount(1)
 })
+
+test('can create multiple preparings to the same node with different prefixes', async ({ page }) => {
+  const nodeName = 'NodeMultiPrefixes'
+  const productName = 'MultiPrefixesProduct'
+  const prefixOne = 'prefix-one'
+  const prefixOther = 'prefix-other'
+
+  const { productId } = await setup(page, nodeName, productName)
+  await addImageToSimpleProduct(page, productId, 'nginx')
+  const one = await addDeploymentToSimpleProduct(page, productId, nodeName, prefixOne)
+  const other = await addDeploymentToSimpleProduct(page, productId, nodeName, prefixOther)
+
+  await page.goto(one.url)
+  await page.waitForSelector(`label:has-text("Prefix: ${prefixOne}")`)
+  await expect(await page.locator(`label:has-text("Prefix: ${prefixOne}")`)).toHaveCount(1)
+
+  await page.goto(other.url)
+  await page.waitForSelector(`label:has-text("Prefix: ${prefixOther}")`)
+  await expect(await page.locator(`label:has-text("Prefix: ${prefixOther}")`)).toHaveCount(1)
+})
