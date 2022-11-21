27c0f856f08ed676ae7eeea71016535241359b46
Bence Nagygyörgy <robot9706@gmail.com>
2022-11-21 14:26:00 +0100

    test(crux-ui): test complex template

--- web/crux-ui/e2e/template.spec.ts
+++ web/crux-ui/e2e/template.spec.ts
@@ -2,10 +2,23 @@ import { productUrl } from '@app/routes'
 import { expect, Page, test } from '@playwright/test'
 import { createProductFromTemplate } from './utils/templates'
 
-test('creating a product from a template should work', async ({ page }) => {
-  const productId = await createProductFromTemplate(page, "Strapi", "Strapi-test", "Simple")
+test('creating a simple product from a template should work', async ({ page }) => {
+  const productId = await createProductFromTemplate(page, 'Strapi', 'Strapi-test-simple', 'Simple')
 
   await expect(page).toHaveURL(productUrl(productId))
+
+  const imageTableBody = await page.locator('.table-row-group')
+  const imageRows = await imageTableBody.locator('.table-row')
+  await expect(imageRows).toHaveCount(2)
+})
+
+test('creating a complex product from a template should work', async ({ page }) => {
+  const productId = await createProductFromTemplate(page, 'Strapi', 'Strapi-test-complex', 'Complex')
+
+  await expect(page).toHaveURL(productUrl(productId))
+
+  await page.locator('text="Images"').click()
+  await page.waitForNavigation()
 
   const imageTableBody = await page.locator(".table-row-group")
   const imageRows = await imageTableBody.locator(".table-row")
