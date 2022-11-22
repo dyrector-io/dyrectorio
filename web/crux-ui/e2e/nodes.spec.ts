24b4ae59ef04efc507d223c5bac92bfe5fe6b0f8
Szabolcs Szigeti <48388277+balanceee@users.noreply.github.com>
2022-11-22 10:23:02 +0100

    test(crux-ui): test deploy with dagent (#355)

--- web/crux-ui/e2e/nodes.spec.ts
+++ web/crux-ui/e2e/nodes.spec.ts
@@ -1,50 +1,6 @@
 import { ROUTE_NODES } from '@app/routes'
 import { expect, test } from '@playwright/test'
-import { exec, ExecOptions } from 'child_process'
 import { screenshotPath } from './utils/common'
-
-// eslint-disable-next-line import/prefer-default-export
-export const deleteContainer = () => {
-  const settings: ExecOptions =
-    process.platform === 'win32'
-      ? {
-          shell: 'C:\\Program Files\\git\\git-bash.exe',
-        }
-      : null
-
-  exec('docker rm -f dagent', settings)
-}
-
-const logCmdOutput = (err: Error, stdOut: string, stdErr: string, logStdOut?: boolean) => {
-  if (logStdOut) {
-    console.info(stdOut)
-  }
-
-  if (err) {
-    console.error(err)
-  }
-
-  if (stdErr) {
-    console.error(stdErr)
-  }
-}
-
-const getInstallScriptExecSettings = (): ExecOptions => {
-  switch (process.platform) {
-    case 'darwin':
-      return {
-        env: { ...process.env },
-      }
-    case 'win32':
-      return {
-        shell: 'C:\\Program Files\\git\\git-bash.exe',
-      }
-    default:
-      return {
-        env: { ...process.env, ROOTLESS: 'true', PERSISTENCE_FOLDER: `${__dirname}/dagent` },
-      }
-  }
-}
 
 test('After adding a new node the setup process should be shown', async ({ page }) => {
   await page.goto(ROUTE_NODES)
@@ -120,36 +76,4 @@ test('Generate script should show the curl command and the script ', async ({ pa
   await expect(timer).toBeVisible()
   await expect(discardButton).toBeVisible()
   await expect(copyButton).toBeVisible()
-})
-
-test('Install dagent should be successful', async ({ page }) => {
-  await page.goto(ROUTE_NODES)
-
-  await page.locator('button:has-text("Add")').click()
-
-  await page.locator('input[name=name] >> visible=true').fill('PW_DAGENT_INSTALL')
-
-  await page.locator('button:has-text("Save")').click()
-
-  await page.waitForSelector('h4:has-text("Technology")')
-
-  const generateScriptButton = await page.locator('button:has-text("Generate script")')
-  await generateScriptButton.click()
-
-  await page.waitForSelector('input[readonly]')
-
-  await page.screenshot({ path: screenshotPath('node-dagent-script'), fullPage: true })
-
-  const commandInput = await page.locator('input[readonly]')
-  const curl = await commandInput.inputValue()
-
-  exec(curl, getInstallScriptExecSettings(), logCmdOutput)
-
-  await page.waitForSelector('img[src="/circle-green.svg"]')
-
-  await page.screenshot({ path: screenshotPath('node-dagent-install-succesful'), fullPage: true })
-
-  await expect(await page.locator('span.text-light-eased:has-text("Running")').first()).toContainText('Running')
-
-  deleteContainer()
 })
