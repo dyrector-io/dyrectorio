343dcf567f9d7dcf36e2a58a8f6bebad4dd8702c
Szabolcs Szigeti <48388277+balanceee@users.noreply.github.com>
2022-11-24 15:08:14 +0100

    test(crux-ui): add e2e tests for delete/copy deployment (#363)

--- web/crux-ui/e2e/utils/common.ts
+++ web/crux-ui/e2e/utils/common.ts
@@ -95,3 +95,16 @@ export const deleteUserByEmail = async (kratos: V0alpha2Api, email: string) => {
 }
 
 export const screenshotPath = (name: string) => path.join(__dirname, '..', SCREENSHOTS_FOLDER, `${name}.png`)
+
+export const extractDeploymentUrl = (url: string): { versionId: string; deploymentId: string } => {
+  const urlParts = url.split('/')
+  urlParts.pop()
+  const deploymentId = urlParts.pop()
+  urlParts.pop()
+  const versionId = urlParts.pop()
+
+  return {
+    versionId,
+    deploymentId,
+  }
+}
