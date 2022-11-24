f9283dae3c3513ab1d393f19573741717dcf4f16
Szilveszter <szilveszter.liszy@dyrector.io>
2022-11-24 13:31:30 +0100

    chore(ci): fix deprecated commands

--- .github/workflows/pipeline_set_output_tag.sh
+++ .github/workflows/pipeline_set_output_tag.sh
@@ -21,5 +21,5 @@ if [ $github_ref_type == "tag" ]; then
   VERSION=$github_ref_name
 fi
 
-echo "::set-output name=tag::$DOCKERIMAGETAG"
-echo "::set-output name=version::$VERSION"
+echo "{tag}={$DOCKERIMAGETAG}" >> $GITHUB_OUTPUT
+echo "{version}={$VERSION}" >> $GITHUB_OUTPUT
