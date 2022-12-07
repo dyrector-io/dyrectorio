ee43e74159cd5e81b3b30227fbf2f8e4edfcbc59
Bence Nagygyörgy <robot9706@gmail.com>
2022-12-07 15:35:34 +0100

    feat(crux-ui): added template image alt

--- web/crux-ui/src/components/templates/template-image.tsx
+++ web/crux-ui/src/components/templates/template-image.tsx
@@ -1,4 +1,5 @@
 import { templateImageUrl } from '@app/routes'
+import useTranslation from 'next-translate/useTranslation'
 import Image from 'next/image'
 import { useState } from 'react'
 
@@ -9,12 +10,20 @@ export interface TemplateImageProps {
 const TemplateImage = (props: TemplateImageProps) => {
   const { templateId } = props
 
+  const { t } = useTranslation('templates')
+
   const [error, setError] = useState<boolean>(false)
 
   return error ? (
     <Image src="/default_template.svg" width={100} height={100} />
   ) : (
-    <img src={templateImageUrl(templateId)} alt="Template" width={100} height={100} onError={() => setError(true)} />
+    <img
+      src={templateImageUrl(templateId)}
+      alt={t('imageAlt')}
+      width={100}
+      height={100}
+      onError={() => setError(true)}
+    />
   )
 }
 
