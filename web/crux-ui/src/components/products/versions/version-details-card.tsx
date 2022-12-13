643132093d034904b7dc86e80a94b26bcdfe1572
Peter Ambrus <peter.ambrus3@gmail.com>
2022-12-13 14:20:30 +0100

    fix(web): deployment and template fix
    fix: shellCommand validation
    fix: deployment overrideArray method
    fix: idify template service image config args
    improvement: dyo expandable text

--- web/crux-ui/src/components/products/versions/version-details-card.tsx
+++ web/crux-ui/src/components/products/versions/version-details-card.tsx
@@ -24,7 +24,7 @@ const VersionDetailsCard = (props: VersionDetailsCardProps) => {
       </DyoHeading>
 
       <div className="flex flex-row">
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full overflow-auto">
           <DyoExpandableText
             text={version.changelog}
             lineClamp={6}
