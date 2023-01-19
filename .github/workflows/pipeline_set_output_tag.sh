25487796d2df087b1b721699cec06e5565afc82b
Szilveszter Liszy <100154938+c3ppc3pp@users.noreply.github.com>
2023-01-19 12:57:58 +0100

    feat(ci): add dockerhub as repo target during image push (#434)

--- .github/workflows/pipeline_set_output_tag.sh
+++ .github/workflows/pipeline_set_output_tag.sh
@@ -11,25 +11,27 @@ DOCKERIMAGETAG="$github_sha"
 VERSION="v0.0.0"
 DEFAULTTAG="latest"
 
-if [ $github_ref_type == "branch" ]; then
+if [ $github_ref_type = "branch" ]; then
   case $github_ref_name in
     "main") DOCKERIMAGETAG="stable"
     ;;
     "develop") DOCKERIMAGETAG="latest"
     ;;
   esac
+
+  if [ $github_base_ref != "" ]; then
+    DOCKERIMAGETAG="latest"
+  fi
+
+  if [ $github_base_ref = "main" ]; then
+    DOCKERIMAGETAG="stable"
+  fi
 fi
 
-if [ $github_base_ref == "main" ]; then
-  DEFAULTTAG="stable"
-fi
-
-if [ $github_ref_type == "tag" ]; then
+if [ $github_ref_type = "tag" ]; then
   DOCKERIMAGETAG=$github_ref_name
   VERSION=$github_ref_name
-  DEFAULTTAG=$github_ref_name
 fi
 
 echo "tag=$DOCKERIMAGETAG" >> $GITHUB_OUTPUT
-echo "fallbacktag=$DEFAULTTAG" >> $GITHUB_OUTPUT
 echo "version=$VERSION" >> $GITHUB_OUTPUT
