22b126951b4ef3949fd4c2d7b010f00df5400612
Nandor Magyar <nandormagyar.it@gmail.com>
2023-01-30 13:30:56 +0100

    fix(crane): add fallback for hostbinding if not defined

--- golang/pkg/crane/k8s/service.go
+++ golang/pkg/crane/k8s/service.go
@@ -125,7 +125,12 @@ func getServicePorts(portBindings []builder.PortBinding, portRanges []builder.Po
 	ports := []*acorev1.ServicePortApplyConfiguration{}
 
 	for i := range portBindings {
-		portNum := int32(*portBindings[i].PortBinding)
+		var portNum int32
+		if portBindings[i].PortBinding != nil {
+			portNum = int32(*portBindings[i].PortBinding)
+		} else {
+			portNum = int32(portBindings[i].ExposedPort)
+		}
 		ports = append(ports,
 			acorev1.ServicePort().
 				WithName(fmt.Sprintf("tcp-%v", portNum)).
