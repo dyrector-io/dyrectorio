8386ccaa7917e5762fb0ec3ca48a55324508fe68
Bence Nagygyörgy <robot9706@gmail.com>
2023-02-01 12:06:00 +0100

    feat: added api token nonce

--- web/crux/prisma/migrations/20230130123810_add_token_table/migration.sql
+++ web/crux/prisma/migrations/20230130123810_add_token_table/migration.sql
@@ -5,9 +5,11 @@ CREATE TABLE "Token" (
     "name" VARCHAR(70) NOT NULL,
     "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "expiresAt" TIMESTAMPTZ(6) NOT NULL,
+    "nonce" UUID NOT NULL,
 
     CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Token_name_key" ON "Token"("name");
+CREATE UNIQUE INDEX "Token_nonce_key" ON "Token"("nonce");
