7175e292fbc17c71d939ce11180bf875cafa21e4
Levente Orban <levente.orban@dyrector.io>
2023-04-13 20:10:26 +0200

    fix: improve the CLI message based on @nandor review

--- golang/pkg/cli/config_file.go
+++ golang/pkg/cli/config_file.go
@@ -192,10 +192,10 @@ func SettingsFileDefaults(initialState *State, args *ArgsFlags) *State {
 		switch {
 		case errors.Is(err, containerRuntime.ErrServerIsOutdated):
 			NotifyOnce("dockerversion", func() {
-				log.Warn().Stack().Err(err).Msg("Docker Engine (Server) is outdated, please consider updating")
+				log.Warn().Stack().Err(err).Msg("There is a newer version of the container engine in use, please consider updating.")
 			})
 		case errors.Is(err, containerRuntime.ErrServerVersionIsNotSupported):
-			log.Fatal().Stack().Err(err).Msg("Docker Engine (Server) is outdated")
+			log.Fatal().Stack().Err(err).Msg("There is a newer version of the container engine in use, please consider updating.")
 		default:
 			log.Fatal().Stack().Err(err).Send()
 		}
