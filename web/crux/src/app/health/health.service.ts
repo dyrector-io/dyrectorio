3a63371b23b5565d5fb7d254d4fe5fb461487d15
Bence <robot9706@gmail.com>
2023-04-17 13:10:46 +0200

    feat(web): health http api (#527)

--- web/crux/src/app/health/health.service.ts
+++ web/crux/src/app/health/health.service.ts
@@ -1,12 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { Configuration, MetadataApi } from '@ory/kratos-client'
-import { NotFoundException } from 'src/exception/errors'
-import { HealthResponse, ServiceStatus } from 'src/grpc/protobuf/proto/crux'
-import { HealthCheckRequest, HealthCheckResponse } from 'src/grpc/protobuf/proto/health'
 import PrismaService from 'src/services/prisma.service'
-import { SERVICE_CRUX, SERVICE_DATABASE, SERVICE_KRATOS } from 'src/shared/models'
-import HealthMapper from './health.mapper'
+import { HealthDto } from './health.dto'
 
 @Injectable()
 export default class HealthService {
@@ -14,14 +10,14 @@ export default class HealthService {
 
   private meta: MetadataApi
 
-  constructor(private prisma: PrismaService, private configService: ConfigService, private mapper: HealthMapper) {
+  constructor(private prisma: PrismaService, private configService: ConfigService) {
     const kratosConfig = new Configuration({
       basePath: this.configService.get<string>('KRATOS_ADMIN_URL'),
     })
     this.meta = new MetadataApi(kratosConfig)
   }
 
-  async getCruxHealth(): Promise<HealthResponse> {
+  async getCruxHealth(): Promise<HealthDto> {
     let lastMigration: string = null
     try {
       lastMigration = await this.prisma.findLastMigration()
@@ -34,58 +30,9 @@ export default class HealthService {
     const version = this.configService.get<string>('npm_package_version')
 
     return {
-      cruxVersion: version,
-      status: lastMigration ? ServiceStatus.OPERATIONAL : ServiceStatus.DISRUPTED,
+      version,
+      status: lastMigration ? 'operational' : 'disrupted',
       lastMigration,
-    }
-  }
-
-  async getHealthCheck(request: HealthCheckRequest): Promise<HealthCheckResponse> {
-    const { service } = request
-
-    const status = await this.getServiceStatus(service)
-
-    return { status: this.mapper.serviceStatusToProto(status) }
-  }
-
-  private async getServiceStatus(service: string): Promise<ServiceStatus> {
-    if (service === SERVICE_KRATOS) {
-      return await this.getKratosHealth()
-    }
-
-    const health = await this.getCruxHealth()
-
-    if (service === SERVICE_DATABASE) {
-      return health.lastMigration ? ServiceStatus.OPERATIONAL : ServiceStatus.UNAVAILABLE
-    }
-
-    if (service === SERVICE_CRUX || !service || service === '') {
-      return health.status
-    }
-
-    throw new NotFoundException({
-      property: 'service',
-      value: service,
-      message: 'Service not found',
-    })
-  }
-
-  private async getKratosHealth(): Promise<ServiceStatus> {
-    try {
-      const readyRes = await this.meta.isReady()
-      if (readyRes.status !== 200) {
-        return ServiceStatus.DISRUPTED
-      }
-
-      const aliveRes = await this.meta.isAlive()
-      if (aliveRes.status !== 200) {
-        return ServiceStatus.UNAVAILABLE
-      }
-
-      return ServiceStatus.OPERATIONAL
-    } catch (err) {
-      this.logger.error(err)
-      return ServiceStatus.UNAVAILABLE
     }
   }
 }
