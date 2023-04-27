f4de0ff8d30d9b77cbe3551c962088f7b960ac0a
Levente Orban <levente.orban@dyrector.io>
2023-04-27 11:13:19 +0200

    feat: resolve conflicts after pull noff

--- web/crux/src/app/team/user.http.controller.ts
+++ web/crux/src/app/team/user.http.controller.ts
@@ -45,9 +45,8 @@ export default class UserHttpController {
 
   @Post()
   @ApiOperation({
-    description:
-      "Response should include an array of `user` `data`, `activeTeamId`, `teams`, and `invitations`.",
-    summary: "Create a new user.",
+    description: 'Response should include an array of `user` `data`, `activeTeamId`, `teams`, and `invitations`.',
+    summary: 'Create a new user.',
   })
   @ApiOkResponse({ type: UserMetaDto, description: 'New user created.' })
   async getUserMeta(@SessionFromRequest() session: Session): Promise<UserMetaDto> {
@@ -57,9 +56,8 @@ export default class UserHttpController {
   @Post(ROUTE_ACTIVE_TEAM)
   @HttpCode(204)
   @ApiOperation({
-    description:
-      "Request must include `teamID`.",
-    summary: "Create a new team.",
+    description: 'Request must include `teamID`.',
+    summary: 'Create a new team.',
   })
   @ApiBody({ type: ActivateTeamDto })
   @ApiNoContentResponse({ description: 'New team created.' })
@@ -69,34 +67,24 @@ export default class UserHttpController {
 
   @Post(`${ROUTE_INVITATIONS}/${ROUTE_TEAM_ID}`)
   @HttpCode(204)
-<<<<<<< HEAD
-  @ApiNoContentResponse({ description: 'Accept invitation to a team.' })
-  @UuidParams(PARAM_TEAM_ID)
-=======
   @ApiOperation({
-    description:
-      "Request must include `teamID`.",
-    summary: "Accept invitation to a team.",
+    description: 'Request must include `teamID`.',
+    summary: 'Accept invitation to a team.',
   })
   @ApiNoContentResponse({ description: 'Invitation accepted.' })
->>>>>>> 5d447b6a (feat(crux): improve openapi docs)
+  @UuidParams(PARAM_TEAM_ID)
   async acceptTeamInvitation(@TeamId() teamId: string, @IdentityFromRequest() identity: Identity): Promise<void> {
     await this.service.acceptTeamInvitation(teamId, identity)
   }
 
   @Delete(`${ROUTE_INVITATIONS}/${ROUTE_TEAM_ID}`)
   @HttpCode(204)
-<<<<<<< HEAD
-  @ApiNoContentResponse({ description: 'Reject invitation to a team.' })
-  @UuidParams(PARAM_TEAM_ID)
-=======
   @ApiOperation({
-    description:
-      "Request must include `teamID`.",
-    summary: "Decline invitation to a team.",
+    description: 'Request must include `teamID`.',
+    summary: 'Decline invitation to a team.',
   })
   @ApiNoContentResponse({ description: 'Invitation declined.' })
->>>>>>> 5d447b6a (feat(crux): improve openapi docs)
+  @UuidParams(PARAM_TEAM_ID)
   async declineTeamInvitation(@TeamId() teamId: string, @IdentityFromRequest() identity: Identity): Promise<void> {
     await this.service.declineTeamInvitation(teamId, identity)
   }
