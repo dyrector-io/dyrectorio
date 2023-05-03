b9896bae901ca2211bb105868a57d2d1e0d792bc
Szilveszter Liszy <100154938+c3ppc3pp@users.noreply.github.com>
2023-05-03 10:44:50 +0200

    feat(ci): validate title of the PR (#557)

---
+++ .github/workflows/title_validation.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+filtered=$(echo $1 | grep -E '(feat|fix|chore|build|docs|refactor|techdebt|release|test|chore|BREAKING CHANGE)(\((web|agent|ci|cli|deps|crux|crux-ui|kratos|crane|dagent)\))?: [\w\S]*')
+
+if [ -n "$filtered" ]; then
+  echo "Title is valid."
+  exit 0
+fi
+
+echo "Title is invalid."
+exit 123
+
