2fe61a0d7bd4d784a88adbdf67cd4bfac289fe4c
Szilveszter Liszy <100154938+c3ppc3pp@users.noreply.github.com>
2023-05-04 10:52:26 +0200

    fix(ci): labeling by title (#571)

--- .github/workflows/title_validation.sh
+++ .github/workflows/title_validation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-filtered=$(echo $1 | grep -E '(feat|fix|chore|build|docs|refactor|techdebt|release|test|chore|BREAKING CHANGE)(\((web|agent|ci|cli|deps|crux|crux-ui|kratos|crane|dagent)\))?: [\w\S]*')
+filtered=$(echo $1 | grep -E '(feat|fix|chore|build|docs|refactor|techdebt|release|test|BREAKING CHANGE)(\((web|agent|ci|cli|deps|crux|crux-ui|kratos|crane|dagent)\))?: [\w\S]*')
 
 if [ -n "$filtered" ]; then
   echo "Title is valid."
