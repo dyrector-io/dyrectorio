ccd951b936986321b0fe1b179524c4728ad288be
Levente Orban <levente.orban@dyrector.io>
2023-05-10 07:49:48 +0200

    feat(crux): redact req.headers.cookie from log

--- web/crux/src/config/pino.logger.config.ts
+++ web/crux/src/config/pino.logger.config.ts
@@ -3,15 +3,12 @@ import pino from 'pino'
 const pinoLoggerConfig = {
   pinoHttp: {
     logger: pino({
-      customProps: () => ({
-        context: 'HTTP',
-      }),
+      mixin: () => ({ context: 'HTTP' }),
       level: process.env.LOG_LEVEL,
-      // Added this for further finetuning
-      // redact: {
-      //   paths: ['req.headers', 'remoteAddress'],
-      //   remove: true,
-      // },
+      redact: {
+        paths: ['req.headers.cookie', 'req.remoteAddress', 'req.remotePort', 'res.headers.etag'],
+        remove: true,
+      },
       transport:
         // Use pino-pretty only in the development environment
         process.env.NODE_ENV !== 'production'
@@ -21,7 +18,7 @@ const pinoLoggerConfig = {
                 singleLine: true,
                 ignore: 'pid,hostname',
                 translateTime: 'mm/dd/yyyy h:MM:ss',
-                messageFormat: '[{req.headers.host}{req.url}][{context}] {msg} [{responseTime} ms][{res.statusCode}]',
+                messageFormat: '[{res.statusCode} | {context} | {req.headers.host}{req.url}] {msg} [{responseTime} ms]',
                 errorLikeObjectKeys: ['err', 'error'],
               },
             }
