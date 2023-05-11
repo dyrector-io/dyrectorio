4f5f16705433f50eaa37ffe69b73bf97c5b9941b
Mate Vago <mate.vago.it@gmail.com>
2023-05-11 14:34:50 +0200

    build: add jq to alpine-proto image

--- .github/workflows/update-package-version.sh
+++ .github/workflows/update-package-version.sh
@@ -7,7 +7,7 @@ commit_hash=$2
 commit_short_hash=$( git rev-parse --short $commit_hash )
 
 version=$( cat $package_json_path | jq -r .version )
-version=$version-$commit_short_hash
+version=${version}-${commit_short_hash}
 
 jq --arg version $version -r '.version |= $version' $package_json_path > new-package.json
 mv new-package.json $package_json_path
