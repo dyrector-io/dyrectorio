52b83c73edecaf823a39bd325b79d0c6ec09a348
Levente Orban <levente.orban@dyrector.io>
2023-05-12 13:36:30 +0200

    feat(crux): add pino logger, and small config refactors (#594)
    Co-authored-by: Mate Vago <mate.vago.it@gmail.com>

--- web/crux/src/shared/const.ts
+++ web/crux/src/shared/const.ts
@@ -1,5 +1,6 @@
 export const REGISTRY_HUB_URL = 'hub.docker.com'
 export const REGISTRY_GITHUB_URL = 'ghcr.io'
+export const PRODUCTION = 'production'
 
 export const REGISTRY_GITLAB_URLS = {
   apiUrl: 'gitlab.com',
