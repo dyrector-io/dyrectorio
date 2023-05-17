0ac48805a44cb07c70e1a623dd10d2b277e833ee
Bence Nagygyörgy <robot9706@gmail.com>
2023-05-17 15:50:17 +0200

    feat(crux-ui): remove unused import

--- web/crux-ui/src/components/main/team-selection-card.tsx
+++ web/crux-ui/src/components/main/team-selection-card.tsx
@@ -7,7 +7,6 @@ import { sendForm } from '@app/utils'
 import clsx from 'clsx'
 import useTranslation from 'next-translate/useTranslation'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
 
 interface TeamSelectionCardProps {
   className?: string
