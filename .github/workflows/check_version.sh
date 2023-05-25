6fd814f91a08d6cea6c3735df446d146b2549958
Mate Vago <mate.vago.it@gmail.com>
2023-05-25 22:16:17 +0200

    build: Add hardcoded agent and cli versions

--- .github/workflows/check_version.sh
+++ .github/workflows/check_version.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
 version=$1
+path=$2
 
-packageversion=$(cat $2/package.json | grep -Eo '\"version\": \"[[:digit:]]+\.[[:digit:]+\.[[:digit:]]+\"')
-formattedversion="\"version\": \"$version\""
-
-if [ "$formattedversion" = "$packageversion" ]; then
-  exit 0
+if [ "${path: -5}" = .json ]; then
+  # json
+  package_version=$(cat $path | grep -Eo '"version": "[[:digit:]]+\.[[:digit:]+\.[[:digit:]]+"')
+  expected_version="\"version\": \"$version\""
+elif [ "${path: -3}" = .go ]; then
+  # go
+  package_version=$(cat $path | grep -Eo 'Version[[:blank:]]+= "[[:digit:]]+\.[[:digit:]+\.[[:digit:]]+"')
+  expected_version="Version = \"$version\""
 else
+  echo Invalid file $path
+  exit 2
+fi
+
+#get rid of unnecessary spaces
+package_version=$(echo $package_version)
+
+if [ "$expected_version" != "$package_version" ]; then
+  echo Invalid "$package_version", expected "$expected_version" in $path
   exit 1
 fi
+
+exit 0
