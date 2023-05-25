b48ded3c79b2717dfe58b0b7243cddac1feeecfc
Mate Vago <mate.vago.it@gmail.com>
2023-05-25 14:02:35 +0200

    test(crux-ui): running e2e tests on cli based dev-stack (#636)

--- web/crux-ui/e2e/utils/global-setup.ts
+++ web/crux-ui/e2e/utils/global-setup.ts
@@ -5,14 +5,18 @@ import { createUser, kratosFromConfig, USER_EMAIL, USER_PASSWORD, USER_TEAM } fr
 import globalTeardown from './global-teardown'
 import { installDagent } from './node-helper'
 
+const logInfo = (...messages: string[]) => console.info('[E2E]: Setup -', ...messages)
+
 const globalSetup = async (config: FullConfig) => {
   await globalTeardown(config)
 
+  logInfo('creating user')
   const kratos = kratosFromConfig(config)
   await createUser(kratos, USER_EMAIL, USER_PASSWORD, {
     verified: true,
   })
 
+  logInfo('launching browser')
   const project = config.projects[0].use
   const { baseURL, storageState } = project
   const browser = await chromium.launch()
@@ -20,22 +24,26 @@ const globalSetup = async (config: FullConfig) => {
     baseURL,
   })
 
+  logInfo('logging in')
   await page.goto(ROUTE_LOGIN)
   await page.locator('input[name=email]').fill(USER_EMAIL)
   await page.locator('input[name=password]').fill(USER_PASSWORD)
   await page.locator('button[type=submit]').click()
 
+  logInfo('creating team')
   await page.waitForURL(ROUTE_TEAMS_CREATE)
   await page.locator('input[name=name]').fill(USER_TEAM)
   await page.locator('button[type=submit]').click()
 
   await page.waitForURL(ROUTE_DASHBOARD)
 
+  logInfo('saving storage state')
   await page.context().storageState({ path: storageState as string })
 
-  // setup a deployable node
+  logInfo('installing dagent')
   await installDagent(page)
 
+  logInfo('closing browser')
   await browser.close()
 }
 
