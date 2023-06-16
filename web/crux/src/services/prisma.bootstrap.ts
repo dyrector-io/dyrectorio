dd0ed8a76a8767a267b87ea2afaa114532bc1b09
Bence Nagygyörgy <robot9706@gmail.com>
2023-06-16 15:32:10 +0200

    fix(crux): typo

--- web/crux/src/services/prisma.bootstrap.ts
+++ web/crux/src/services/prisma.bootstrap.ts
@@ -5,7 +5,7 @@ import PrismaService from './prisma.service'
 const prismaBootstrap = async (app: INestApplication) => {
   const prisma = await app.get(PrismaService)
 
-  // Crux is starting so imProgress deployments should not exist
+  // Crux is starting so inProgress deployments should not exist
   // Find inProgress deployments and mark them as failed
   await prisma.deployment.updateMany({
     where: {
