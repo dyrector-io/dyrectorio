15d3c0e2fa83001f0607ec05ce62d103a51a2a85
Bence Nagygyörgy <robot9706@gmail.com>
2023-06-26 15:33:35 +0200

    fix(crux-ui): lint

--- web/crux-ui/src/validations/token.ts
+++ web/crux-ui/src/validations/token.ts
@@ -4,11 +4,10 @@ import { nameRule } from './common'
 // eslint-disable-next-line import/prefer-default-export
 export const generateTokenSchema = yup.object().shape({
   name: nameRule.required(),
-  expirationInDays: yup
-    .mixed()
-    .test(
-      'is-token-expiration',
-      '${label} is not a valid token expiration',
-      value => value === 'never' || typeof value === 'number',
-    ),
+  expirationInDays: yup.mixed().test(
+    'is-token-expiration',
+    // eslint-disable-next-line no-template-curly-in-string
+    '${label} is not a valid token expiration',
+    value => value === 'never' || typeof value === 'number',
+  ),
 })
