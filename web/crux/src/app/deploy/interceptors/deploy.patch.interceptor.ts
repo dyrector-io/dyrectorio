da7e20589158b98b9bb592598d4488bfeff7acb7
Bence Nagygyörgy <robot9706@gmail.com>
2023-07-21 14:17:55 +0200

    feat(crux-ui): deployment patch prefix error

--- web/crux/src/app/deploy/interceptors/deploy.patch.interceptor.ts
+++ web/crux/src/app/deploy/interceptors/deploy.patch.interceptor.ts
@@ -1,8 +1,10 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
 import { Observable } from 'rxjs'
 import { checkDeploymentMutability } from 'src/domain/deployment'
-import { CruxPreconditionFailedException } from 'src/exception/crux-exception'
+import { CruxConflictException, CruxPreconditionFailedException } from 'src/exception/crux-exception'
 import PrismaService from 'src/services/prisma.service'
+import { PatchDeploymentDto } from '../deploy.dto'
+import { VersionTypeEnum } from '@prisma/client'
 
 @Injectable()
 export default class DeployPatchValidationInterceptor implements NestInterceptor {
@@ -10,6 +12,8 @@ export default class DeployPatchValidationInterceptor implements NestInterceptor
 
   async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
     const req = context.switchToHttp().getRequest()
+    const body = req.body as PatchDeploymentDto
+
     const deploymentId = req.params.deploymentId as string
     const deployment = await this.prisma.deployment.findUniqueOrThrow({
       where: {
@@ -25,6 +29,37 @@ export default class DeployPatchValidationInterceptor implements NestInterceptor
         message: 'Invalid deployment status.',
         property: 'status',
         value: deployment.status,
+      })
+    }
+
+    const existingDeployment = await this.prisma.deployment.findFirst({
+      where: {
+        id: {
+          not: deploymentId,
+        },
+
+        nodeId: deployment.nodeId,
+        versionId: deployment.versionId,
+        prefix: body.prefix,
+
+        // Rolling versions can only have one deployment for a node with the same prefix
+        status: deployment.version.type === VersionTypeEnum.rolling ? undefined : 'preparing',
+      },
+      select: {
+        id: true,
+      },
+    })
+
+    if (existingDeployment) {
+      throw new CruxConflictException({
+        message:
+          deployment.version.type === VersionTypeEnum.rolling
+            ? 'Rolling versions can only have one deployment for the same node with the same prefix!'
+            : 'You already have one preparing deployment with the same prefix!',
+        error:
+          deployment.version.type === VersionTypeEnum.rolling ? 'rollingVersionDeployment' : 'alreadyHavePreparing',
+        property: 'deploymentId',
+        value: existingDeployment.id,
       })
     }
 
