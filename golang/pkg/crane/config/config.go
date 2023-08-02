a4bef87c035f4517737d9e8decd5acf04f074682
Nandor Magyar <nandormagyar.it@gmail.com>
2023-08-02 11:35:55 +0200

    fix(agent): add create directory healthcheck creation

--- golang/pkg/crane/config/config.go
+++ golang/pkg/crane/config/config.go
@@ -17,7 +17,7 @@ type Configuration struct {
 	KubeConfig          string        `yaml:"kubeConfig"            env:"KUBECONFIG"                env-default:""`
 	TestTimeoutDuration time.Duration `yaml:"testTimeout"           env:"TEST_TIMEOUT"              env-default:"15s"`
 	OwnDeployment       string        `yaml:"ownDeployment"         env:"CRANE_DEPLOYMENT_NAME"`
-	OwnNamespace        string        `yaml:"ownNamescae"           env:"CRANE_DEPLOYMENT_NAMESPACE"`
+	OwnNamespace        string        `yaml:"ownNamespace"           env:"CRANE_DEPLOYMENT_NAMESPACE"`
 	// for injecting SecretPrivateKey
 	SecretName string `yaml:"secretName"  env:"SECRET_NAME"         env-default:"dyrectorio-secret"`
 	Namespace  string `yaml:"namespace"   env:"SECRET_NAMESPACE"    env-default:"dyrectorio"`
