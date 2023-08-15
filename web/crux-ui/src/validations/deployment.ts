8b4d5666127d884572eccfa87a96d313f663c776
ngyradi <138432249+ngyradi@users.noreply.github.com>
2023-08-15 13:31:20 +0200

    feat(crux-ui): deployment page create card (#782)

--- web/crux-ui/src/validations/deployment.ts
+++ web/crux-ui/src/validations/deployment.ts
@@ -1,13 +1,15 @@
 import * as yup from 'yup'
 import { nameRule } from './common'
 
+const prefixRule = yup
+  .string()
+  .trim()
+  .matches(/^[a-z0-9]([-a-z0-9]*[a-z0-9])?$/) // RFC 1123
+  .required()
+
 export const updateDeploymentSchema = yup.object().shape({
   note: yup.string(),
-  prefix: yup
-    .string()
-    .trim()
-    .matches(/^[a-z0-9]([-a-z0-9]*[a-z0-9])?$/) // RFC 1123
-    .required(),
+  prefix: prefixRule,
 })
 
 export const createDeploymentSchema = updateDeploymentSchema.concat(
@@ -21,4 +23,12 @@ export const copyDeploymentSchema = createDeploymentSchema
 export const createDeploymentTokenSchema = yup.object().shape({
   name: nameRule,
   expirationInDays: yup.number().nullable(),
+})
+
+export const createFullDeploymentSchema = yup.object().shape({
+  nodeId: yup.string().required(),
+  prefix: prefixRule,
+  versionId: yup.string().required(),
+  projectId: yup.string().required(),
+  note: yup.string().nullable().optional(),
 })
