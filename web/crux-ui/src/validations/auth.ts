01200ef542e86f30cb1ebe73711780a0a840a06c
ngyradi <138432249+ngyradi@users.noreply.github.com>
2023-08-21 11:55:25 +0200

    fix(crux-ui): register and team card validation (#784)

--- web/crux-ui/src/validations/auth.ts
+++ web/crux-ui/src/validations/auth.ts
@@ -1,22 +1,43 @@
 import { OidcProvider, OIDC_PROVIDER_VALUES } from '@app/models'
 import * as yup from 'yup'
-import { identityNameRule } from './common'
+import { identityNameRule, passwordLengthRule } from './common'
 
 export const registerWithPasswordSchema = yup.object().shape({
+  password: passwordLengthRule.required(),
   email: yup.string().email().required(),
-  password: yup.string().required(),
   firstName: identityNameRule.required(),
   lastName: identityNameRule.min(0),
 })
 
 export const registerWithOidcSchema = yup.object().shape({
   provider: yup.mixed<OidcProvider>().oneOf([...OIDC_PROVIDER_VALUES]),
+  firstName: identityNameRule.required(),
+  lastName: identityNameRule.min(0),
+  email: yup.string().email().required(),
 })
 
-export const registerSchema = yup.object().when('method', {
-  is: 'password',
-  then: () => registerWithPasswordSchema,
-  otherwise: () => registerWithOidcSchema,
+export const registerSchema = yup.object().shape({
+  method: yup.string().required(),
+  password: yup.string().when('method', {
+    is: 'password',
+    then: () => passwordLengthRule.required(),
+  }),
+  email: yup.string().when('method', {
+    is: 'password',
+    then: () => yup.string().email().required(),
+  }),
+  firstName: identityNameRule.when('method', {
+    is: 'password',
+    then: () => identityNameRule.required(),
+  }),
+  lastName: identityNameRule.when('method', {
+    is: 'password',
+    then: () => identityNameRule.min(0),
+  }),
+  provider: yup.mixed<OidcProvider>().when('method', {
+    is: 'oidc',
+    then: () => yup.mixed<OidcProvider>().oneOf([...OIDC_PROVIDER_VALUES]),
+  }),
 })
 
 export const verifyEmailSchema = yup.object().shape({
@@ -34,8 +55,8 @@ export const passwordSchema = yup.object().shape({
 
 export const userProfileSchema = yup.object().shape({
   email: yup.string().email().required(),
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
+  firstName: identityNameRule.required(),
+  lastName: identityNameRule.min(0),
 })
 
 export const recoverySchema = yup.object().shape({
