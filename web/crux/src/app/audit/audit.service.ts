233f2a300f21ce2c50535147ceff505c555a0099
Bence Nagygyörgy <robot9706@gmail.com>
2023-10-17 14:13:21 +0200

    fix(crux): prefilter audit users

--- web/crux/src/app/audit/audit.service.ts
+++ web/crux/src/app/audit/audit.service.ts
@@ -5,7 +5,7 @@ import { IdentityTraits, nameOfIdentity } from 'src/domain/identity'
 import KratosService from 'src/services/kratos.service'
 import PrismaService from 'src/services/prisma.service'
 import { AuditLogListDto, AuditLogQueryDto } from './audit.dto'
-import AuditMapper, { AuditLogWithDeploymentToken } from './audit.mapper'
+import AuditMapper from './audit.mapper'
 
 @Injectable()
 export default class AuditService {
@@ -16,9 +16,9 @@ export default class AuditService {
   ) {}
 
   async getAuditLog(teamSlug: string, query: AuditLogQueryDto): Promise<AuditLogListDto> {
-    const { skip, take, from, to, filter } = query
+    const { skip, take, from, to, filter: queryFilter } = query
 
-    const where: Prisma.AuditLogWhereInput = {
+    let where: Prisma.AuditLogWhereInput = {
       team: {
         slug: teamSlug,
       },
@@ -26,6 +26,51 @@ export default class AuditService {
         gte: from,
         lte: to,
       },
+    }
+
+    let identities: Map<string, Identity> = null
+
+    if (queryFilter) {
+      const filter = queryFilter.toLowerCase()
+
+      const users = await this.prisma.auditLog.findMany({
+        where,
+        select: {
+          userId: true,
+        },
+        distinct: ['userId'],
+      })
+
+      identities = await this.kratos.getIdentitiesByIds(new Set(users.map(it => it.userId)))
+
+      const filteredIdentities = Array.from(identities.values())
+        .filter(it => {
+          const traits = it.traits as IdentityTraits
+          if (traits.email.toLocaleLowerCase().includes(filter)) {
+            return true
+          }
+
+          const name = nameOfIdentity(it)
+          return name.toLowerCase().includes(filter)
+        })
+        .map(it => it.id)
+
+      where = {
+        ...where,
+        OR: [
+          {
+            userId: {
+              in: filteredIdentities,
+            },
+          },
+          {
+            event: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      }
     }
 
     const [auditLog, total] = await this.prisma.$transaction([
@@ -47,42 +92,13 @@ export default class AuditService {
       this.prisma.auditLog.count({ where }),
     ])
 
-    const identities = await this.kratos.getIdentitiesByIds(new Set(auditLog.map(it => it.userId)))
-
-    if (filter) {
-      const filtered = this.filterAuditLog(auditLog, filter, identities)
-      return {
-        items: filtered.map(it => this.mapper.toDetailsDto(it, identities)),
-        total: filtered.length,
-      }
+    if (!queryFilter) {
+      identities = await this.kratos.getIdentitiesByIds(new Set(auditLog.map(it => it.userId)))
     }
 
     return {
       items: auditLog.map(it => this.mapper.toDetailsDto(it, identities)),
       total,
     }
-  }
-
-  private filterAuditLog(
-    items: AuditLogWithDeploymentToken[],
-    filter: string,
-    identities: Map<string, Identity>,
-  ): AuditLogWithDeploymentToken[] {
-    const filterLowercase = filter.toLowerCase()
-    return items.filter(it => {
-      if (it.event.toLowerCase().includes(filterLowercase)) {
-        return true
-      }
-
-      const identity = identities.get(it.userId)
-      if (identity) {
-        const name = nameOfIdentity(identity).toLowerCase()
-        const traits = identity.traits as IdentityTraits
-
-        return name.includes(filterLowercase) || traits.email.toLowerCase().includes(filterLowercase)
-      }
-
-      return false
-    })
   }
 }
