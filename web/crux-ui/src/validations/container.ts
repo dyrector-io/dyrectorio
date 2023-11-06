94407f4244bbf4e6c558c2d87af36b62be77afa0
Mate Vago <mate.vago.it@gmail.com>
2023-11-06 11:31:37 +0100

    feat: add working directory to container config (#879)
    Co-authored-by: Bence Nagygyörgy <robot9706@gmail.com>

--- web/crux-ui/src/validations/container.ts
+++ web/crux-ui/src/validations/container.ts
@@ -404,6 +404,7 @@ const containerConfigBaseSchema = yup.object().shape({
   routing: routingRule,
   expose: exposeRule,
   user: yup.number().default(null).min(-1).max(UID_MAX).nullable().label('container:common.user'),
+  workingDirectory: yup.string().default(null).nullable().optional().label('container:common.workingDirectory'),
   tty: yup.boolean().default(false).required().label('container:common.tty'),
   configContainer: configContainerRule,
   ports: portConfigRule,
