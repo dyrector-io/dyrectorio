46ec087cacb7f7f6758d92f472d92aafbbd51c0c
chandhu <chandhudev0@gmail.com>
2023-11-28 17:58:06 +0530

    fix(crux-ui): fixes the linter issues

--- web/crux-ui/src/elements/dyo-password.tsx
+++ web/crux-ui/src/elements/dyo-password.tsx
@@ -2,22 +2,27 @@ import React from 'react'
 import DyoIcon from './dyo-icon'
 import useTranslation from 'next-translate/useTranslation'
 
-
 export type passwordIconProps = {
-    onClick : VoidFunction,
-    isVisible : boolean
+  onClick: VoidFunction
+  isVisible: boolean
 }
 
-export const DyoPassword = ((props: passwordIconProps) => {
-    const { onClick, isVisible } = props
-    const { t } = useTranslation('common')
-    
-return (
+const DyoPassword = (props: passwordIconProps) => {
+  const { onClick, isVisible } = props
+  const { t } = useTranslation('common')
+
+  return (
     <>
-     {
-      <DyoIcon src={isVisible ? `/eye_close.svg` : '/eye.svg'} alt={isVisible ? t(`hidePassword`): t('showPassword')} size={'md'} onClick={onClick} />
-     }
+      {
+        <DyoIcon
+          src={isVisible ? `/eye_close.svg` : '/eye.svg'}
+          alt={isVisible ? t(`hidePassword`) : t('showPassword')}
+          size={'md'}
+          onClick={onClick}
+        />
+      }
     </>
-)})
+  )
+}
 
-
+export default DyoPassword
