6fd69e874f02ee1c3fd31458a4b2d2cfd842a21d
Nandor Magyar <nandormagyar.it@gmail.com>
2024-01-25 15:04:03 +0100

    test(agent): add fuzz testing for docker version checks

--- golang/internal/runtime/container/container_test.go
+++ golang/internal/runtime/container/container_test.go
@@ -7,6 +7,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
+	"regexp"
+	"strconv"
 	"testing"
 
 	containerRuntime "github.com/dyrector-io/dyrectorio/golang/internal/runtime/container"
@@ -76,68 +79,105 @@ func getDockerInfoInvalid() *types.Info {
 type VersionTestCase struct {
 	Info              *types.Info
 	MockClientVersion string
-	ErrExpected       error
 }
 
-func TestVersionCheck(t *testing.T) {
-	errExternal := errors.New("externalError")
-	testCases := []VersionTestCase{
+type VersionTestCaseWithErr struct {
+	VersionTestCase
+	ErrExpected error
+}
+
+var (
+	errExternal = errors.New("externalError")
+	testCases   = []VersionTestCase{
 		// no errors
 		{
 			Info:              getDockerInfoDocker(),
 			MockClientVersion: "23.0.0",
-			ErrExpected:       nil,
+		},
+		{
+			Info:              getDockerInfoDocker(),
+			MockClientVersion: "24.0.0",
 		},
 		{
 			Info:              getDockerInfoPodman(),
 			MockClientVersion: "4.4.0",
-			ErrExpected:       nil,
 		},
+		{
+			Info:              getDockerInfoPodman(),
+			MockClientVersion: "4.8.0",
+		},
+	}
+	testCasesErrnous = []VersionTestCaseWithErr{
 		// server outdated errors
 		{
-			Info:              getDockerInfoDocker(),
-			MockClientVersion: "20.10.0",
-			ErrExpected:       containerRuntime.ErrServerIsOutdated,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoDocker(),
+				MockClientVersion: "20.10.0",
+			},
+			ErrExpected: containerRuntime.ErrServerIsOutdated,
 		},
 		{
-			Info:              getDockerInfoPodman(),
-			MockClientVersion: "4.0.0",
-			ErrExpected:       containerRuntime.ErrServerIsOutdated,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoPodman(),
+				MockClientVersion: "4.0.0",
+			},
+			ErrExpected: containerRuntime.ErrServerIsOutdated,
 		},
 		{
-			Info:              getDockerInfoDocker(),
-			MockClientVersion: "19.03.0",
-			ErrExpected:       containerRuntime.ErrServerVersionIsNotSupported,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoDocker(),
+				MockClientVersion: "19.03.0",
+			},
+			ErrExpected: containerRuntime.ErrServerVersionIsNotSupported,
 		},
 		{
-			Info:              getDockerInfoPodman(),
-			MockClientVersion: "3.4.0",
-			ErrExpected:       containerRuntime.ErrServerVersionIsNotSupported,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoPodman(),
+				MockClientVersion: "3.4.0",
+			},
+			ErrExpected: containerRuntime.ErrServerVersionIsNotSupported,
 		},
 		{
-			Info:              getDockerInfoInvalid(),
-			MockClientVersion: "23.0.0",
-			ErrExpected:       containerRuntime.ErrServerUnknown,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoInvalid(),
+				MockClientVersion: "23.0.0",
+			},
+			ErrExpected: containerRuntime.ErrServerUnknown,
 		},
 		{
-			Info:              getDockerInfoInvalid(),
-			MockClientVersion: "4.4.0",
-			ErrExpected:       containerRuntime.ErrServerUnknown,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoInvalid(),
+				MockClientVersion: "4.4.0",
+			},
+			ErrExpected: containerRuntime.ErrServerUnknown,
 		},
 		// We don't check the specific error there, since it is not covered by the package but by an external dependency.
 		{
-			Info:              getDockerInfoDocker(),
-			MockClientVersion: "a.b.c",
-			ErrExpected:       errExternal,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoDocker(),
+				MockClientVersion: "a.b.c",
+			},
+			ErrExpected: errExternal,
 		},
 		{
-			Info:              getDockerInfoDocker(),
-			MockClientVersion: "a.b.c",
-			ErrExpected:       errExternal,
+			VersionTestCase: VersionTestCase{
+				Info:              getDockerInfoPodman(),
+				MockClientVersion: "a.b.c",
+			},
+			ErrExpected: errExternal,
 		},
 	}
+)
 
+func TestVersionCheck(t *testing.T) {
 	for _, tC := range testCases {
+		_, err := containerRuntime.VersionCheck(context.TODO(), newMockClient(tC.MockClientVersion, tC.Info))
+		assert.Nilf(t, err, "unexpected error for valid version input: %s", tC.MockClientVersion)
+	}
+}
+
+func TestVersionCheckNegative(t *testing.T) {
+	for _, tC := range testCasesErrnous {
 		_, err := containerRuntime.VersionCheck(context.TODO(), newMockClient(tC.MockClientVersion, tC.Info))
 		// if the we expect external error there is no strict error matching
 		if errors.Is(tC.ErrExpected, errExternal) {
@@ -145,6 +185,81 @@ func TestVersionCheck(t *testing.T) {
 		} else {
 			assert.ErrorIs(t, err, tC.ErrExpected)
 		}
+	}
+}
+
+// not pretty, but constants should only be changed at one place
+func getMajorMinor(f *testing.F, versionStr string) (uint16, uint16) {
+	re, regErr := regexp.Compile(`(\d+)`)
+	if regErr != nil {
+		f.Fatal("error compiling regular expression")
+	}
+	res := re.FindAllString(versionStr, -1)
+
+	major, err := strconv.ParseUint(res[0], 10, 16)
+	if err != nil {
+		f.Fatal(err)
+	}
+	minor, err := strconv.ParseUint(res[1], 10, 16)
+	if err != nil {
+		f.Fatal(err)
+	}
+	return uint16(major), uint16(minor)
+}
+
+func fuzzVersionWithRuntime(f *testing.F, dockerInfo *types.Info, majorMin, minorMin uint16) func(t *testing.T, major, minor, patch uint16) {
+	return func(t *testing.T, major, minor, patch uint16) {
+		versionStr := fmt.Sprintf("%d.%d.%d", major, minor, patch)
+
+		_, err := containerRuntime.VersionCheck(context.TODO(), newMockClient(versionStr, dockerInfo))
+		if major < majorMin || (major <= majorMin && minor < minorMin) {
+			assertAnyError(t, err, versionStr,
+				containerRuntime.ErrServerIsOutdated,
+				containerRuntime.ErrServerUnknown,
+				containerRuntime.ErrServerVersionIsNotSupported,
+				containerRuntime.ErrServerVersionIsNotValid,
+				containerRuntime.ErrCannotParseServerVersion,
+				containerRuntime.ErrCannotSatisfyVersionConstraint,
+				containerRuntime.ErrCannotParseVersionConstraint,
+			)
+		} else {
+			assert.Nilf(t, err, "error unexpected if over required constants: %s, err:%s", versionStr, err)
+		}
+	}
+}
+
+func FuzzVersionCheckPodman(f *testing.F) {
+	f.Add(uint16(1), uint16(0), uint16(0))
+	f.Add(uint16(20), uint16(0), uint16(10))
+	f.Add(uint16(4), uint16(0), uint16(0))
+	majorMin, minorMin := getMajorMinor(f, containerRuntime.RecommendedPodmanServerVersion)
+	dockerInfo := &types.Info{}
+	f.Fuzz(fuzzVersionWithRuntime(f, dockerInfo, majorMin, minorMin))
+}
+
+func FuzzVersionCheckDocker(f *testing.F) {
+	f.Add(uint16(1), uint16(0), uint16(0))
+	f.Add(uint16(20), uint16(0), uint16(10))
+	f.Add(uint16(21), uint16(0), uint16(10))
+	f.Add(uint16(4), uint16(0), uint16(0))
+	dockerInfo := &types.Info{
+		InitBinary: "docker-init",
+	}
+	majorMin, minorMin := getMajorMinor(f, containerRuntime.RecommendedDockerServerVersion)
+	f.Fuzz(fuzzVersionWithRuntime(f, dockerInfo, majorMin, minorMin))
+}
+
+func assertAnyError(t *testing.T, err error, input string, errs ...error) {
+	for _, e := range errs {
+		if errors.Is(err, e) {
+			log.Printf("the error asserted: %s", e.Error())
+			return
+		}
+	}
+	if err == nil {
+		t.Fatalf("error is nil and it is not in the expected error list, input: %s", input)
+	} else {
+		t.Fatalf("error is included in expected: %s for input: %s", err.Error(), input)
 	}
 }
 
