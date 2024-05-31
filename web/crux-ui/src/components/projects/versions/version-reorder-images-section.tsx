d7208d90756c80dbe06a819beb74651203bc8163
Mate Vago <mate.vago.it@gmail.com>
2024-05-31 14:13:13 +0200

    fix(crux-ui): image ordering (#979)

--- web/crux-ui/src/components/projects/versions/version-reorder-images-section.tsx
+++ web/crux-ui/src/components/projects/versions/version-reorder-images-section.tsx
@@ -5,7 +5,36 @@ import clsx from 'clsx'
 import useTranslation from 'next-translate/useTranslation'
 import { useState } from 'react'
 
-interface VersionReorderImagesSectionProps {
+const sortImages = (images: VersionImage[], order: Map<string, number>): VersionImage[] => {
+  const sorted = images.sort((one, other) => {
+    const oneOrder = order.get(one.id)
+    const otherOrder = order.get(other.id)
+
+    if (oneOrder !== undefined) {
+      return otherOrder !== undefined ? oneOrder - otherOrder : -1
+    }
+
+    return otherOrder !== undefined ? 1 : 0
+  })
+
+  return sorted
+}
+
+const orderImagesByIndex = (images: VersionImage[], newOrder: VersionImage[]): VersionImage[] => {
+  const order: Map<string, number> = new Map()
+  newOrder.forEach((it, index) => order.set(it.id, index))
+
+  return sortImages(images, order)
+}
+
+const orderImages = (images: VersionImage[]): VersionImage[] => {
+  const order: Map<string, number> = new Map()
+  images.forEach(it => order.set(it.id, it.order))
+
+  return sortImages(images, order)
+}
+
+type VersionReorderImagesSectionProps = {
   images: VersionImage[]
   saveRef: React.MutableRefObject<VoidFunction>
   onSave: (images: VersionImage[]) => void
@@ -16,24 +45,10 @@ const VersionReorderImagesSection = (props: VersionReorderImagesSectionProps) =>
 
   const { t } = useTranslation('images')
 
-  const [items, setItems] = useState(images)
+  const [items, setItems] = useState(orderImages(images))
 
   saveRef.current = () => {
-    const currentImages = images
-
-    const order: Map<string, number> = new Map()
-    items.forEach((it, index) => order.set(it.id, index))
-
-    const sorted = currentImages.sort((one, other) => {
-      const oneOrder = order.get(one.id)
-      const otherOrder = order.get(other.id)
-
-      if (oneOrder !== undefined) {
-        return otherOrder !== undefined ? oneOrder - otherOrder : -1
-      }
-
-      return otherOrder !== undefined ? 1 : 0
-    })
+    const sorted = orderImagesByIndex(images, items)
 
     onSave(sorted)
   }
