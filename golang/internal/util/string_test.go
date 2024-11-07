5c155b044e59a09fdefaf9e18e23bce2cc6c1ed5
Nandor Magyar <nandormagyar.it@gmail.com>
2024-11-07 11:47:15 +0100

    fix(dagent): adjust of env variables, empty string as valid value (#1009)

--- golang/internal/util/string_test.go
+++ golang/internal/util/string_test.go
@@ -9,15 +9,20 @@ import (
 )
 
 func TestJoinV(t *testing.T) {
-	assert.Equal(t, util.JoinV(""), "")
-	assert.Equal(t, util.JoinV(":", "o", "o"), "o:o")
-	assert.Equal(t, util.JoinV("\\", "i"), "i")
-	assert.Equal(t, util.JoinV("/", "i", "i", "i"), "i/i/i")
+	assert.Equal(t, "", util.JoinV(""))
+	assert.Equal(t, "o:o", util.JoinV(":", "o", "o"))
+	assert.Equal(t, "i", util.JoinV("\\", "i"))
+	assert.Equal(t, "i/i/i", util.JoinV("/", "i", "i", "i"))
+	assert.Equal(t, "ENVKEY", util.JoinV("=", "ENVKEY", "")) // this is intentionally a bad example
+}
+
+func TestJoinVEmpty(t *testing.T) {
+	assert.Equal(t, "ENVKEY=", util.JoinVEmpty("=", "ENVKEY", "")) // this is intentionally a bad example
 }
 
 func TestFallback(t *testing.T) {
-	assert.Equal(t, util.Fallback(""), "")
-	assert.Equal(t, util.Fallback("1", "2", "3"), "1")
-	assert.Equal(t, util.Fallback("", "2"), "2")
-	assert.Equal(t, util.Fallback("", "", "", "4"), "4")
+	assert.Equal(t, "", util.Fallback(""))
+	assert.Equal(t, "1", util.Fallback("1", "2", "3"))
+	assert.Equal(t, "2", util.Fallback("", "2"))
+	assert.Equal(t, "4", util.Fallback("", "", "", "4"))
 }
