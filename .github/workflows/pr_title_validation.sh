f447cac506ffaa18a4d7275d2674a86907652e7c
Levente Orban <orban.levi.94@gmail.com>
2025-03-17 11:20:37 +0100

    fix(ci): remove unused workflows and rename existings for better usage (#1048)
    * fix(ci): remove unused workflows and rename existings for better usage
    
    * fix(ci): remove unused workflows and rename existings for better usage
    
    * fix(ci): refactor PR Title validation

--- .github/workflows/pr_title_validation.sh
+++ .github/workflows/pr_title_validation.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-filtered=$(echo $1 | grep -E '(cicd|chore|doc|feat|fix|hotfix|refactor|test|BREAKING CHANGE)(\((web|agent|ci|cli|deps|deps-dev|crux|crux-ui|kratos|crane|dagent)\))?: [\w\S]*')
+COMMIT_TYPES="cicd|chore|doc|feat|fix|hotfix|refactor|test|BREAKING CHANGE"
+REPOSITORY_SCOPES="web|agent|ci|cli|deps|deps-dev|crux|crux-ui|kratos|crane|dagent"
+COMMIT_MESSAGE="$1"
 
-if [ -n "$filtered" ]; then
+# Commit message format: COMMIT_TYPES(REPOSITORY_SCOPES): message
+COMMIT_FORMAT="($COMMIT_TYPES)(\\(($REPOSITORY_SCOPES)\\))?: [\\w\\S]*"
+
+if echo "$COMMIT_MESSAGE" | grep -E "$COMMIT_FORMAT" >/dev/null; then
   echo "Title is valid."
   exit 0
+else
+  echo "Title is invalid."
+  exit 1
 fi
-
-echo "Title is invalid."
-exit 123
